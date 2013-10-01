6560ffa37286d5add172a2e42ac2f027f7b6bf8e
Basil Veerman <bveerman@uvic.ca>
2013-10-01 14:51:14 -0700

    Remove menu open on load, it's broken

--- pdp/static/js/canada_ex_app.js
+++ pdp/static/js/canada_ex_app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     map = init_raster_map();
-    amenu.open(ncwms.params.LAYERS, true);
     var selector = document.getElementById("pdp-controls").appendChild(getRasterControls());
     var download = document.getElementById("pdp-controls").appendChild(getRasterDownloadOptions());
 
