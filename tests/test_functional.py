094016436feb6011b9d64551c042a99477c8a8dd
James Hiebert <hiebert@uvic.ca>
2013-10-16 10:30:50 -0700

    Testing work: + moved authorized_session_id fixture into conftest to be used by everything + added a test to check for the existence of a prism data set + added a test for the dds response of one of the BC downscaling datasets

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,6 +1,3 @@
-import random
-import cPickle
-import re
 from datetime import datetime, timedelta
 from urllib import urlencode
 from tempfile import TemporaryFile, NamedTemporaryFile
@@ -18,32 +15,6 @@ import netCDF4
 import numpy as np
 from numpy.testing import assert_almost_equal
 
-
-@pytest.fixture(scope="module")
-def authorized_session_id(check_auth_app):
-    # FIXME: I shouldn't have to do this, but the store doesn't get initialized until the first request
-    oid_app = check_auth_app.wrap_app
-    try:
-        oid_app({}, None)
-    except:
-        pass
-    
-    assoc_handle = 'handle'
-    saved_assoc = 'saved'
-    claimed_id = 'test_id'
-    oid_app.store.add_association(claimed_id, None, saved_assoc)
-    oid_app.store.add_association(assoc_handle, None, saved_assoc)
-
-    session = str(random.getrandbits(40))
-    oid_app.store.start_login(session, cPickle.dumps((claimed_id, assoc_handle)))
-
-    # Simulate the return from the openid provider
-    req = Request.blank('/?openid_return='+session+'&openid.signed=yes')
-    resp = req.get_response(check_auth_app)
-    assert resp.status == '200 OK'
-
-    m = re.search(r'beaker.session.id=([a-f0-9]+);', resp.headers['Set-cookie'])
-    return m.group(1)
 
 @pytest.mark.parametrize('url', ['/', '/pcds_map/', '/js/crmp_map.js', '/css/ie7.css', '/images/banner.png', '/apps/count_stations/'])
 def test_no_404s(pcic_data_portal, url):
