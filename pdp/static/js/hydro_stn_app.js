71c98ba9e25eeec8b485dcb35ee2b752e38688f3
Basil Veerman <bveerman@uvic.ca>
2014-09-05 15:38:50 -0700

    Split some functionality to controls module, resolve scoped varaible issues

--- pdp/static/js/hydro_stn_app.js
+++ pdp/static/js/hydro_stn_app.js
@@ -9,51 +9,17 @@ $(document).ready(function() {
     var selectionList = {};
     var dataArray;
 
-    // Add an item from the Selection sidebar
-    var addToSidebar = function(idx) {
-       var item = pdp.createDiv('stnNo' + idx, '');
-       item.textContent = dataArray[idx].StationName;
-       $('#selectedStations').append(item);
-   };
-
-    // Remove an item from the Selection sidebar
-    var removeFromSidebar = function(idx) {
-        $('#stnNo' + idx).remove();
-    }
-
-    // Create a search box using jquery ui's autocomplete control.
-    var createSearchBox = function(id, cssClass, data, select_callback) {
-        var frag = document.createDocumentFragment();
-        var elem = frag.appendChild(pdp.createInputElement("text", cssClass, id, id, ''));
-        $(elem).autocomplete({ source: data, select: select_callback, delay: 100, minLength: 2 });
-        return frag;
-    };
-
-    // Toggle whether an index into the data array is selected.
-    var toggleIdxSelection = function(idx, icon) {
-	var stnLayer = map.getLayersByName("Stations")[0];
-	
-	if(idx in selectionList) { 
-	    icon.setUrl(pdp.app_root + "/images/mini_triangle.png");
-	    delete selectionList[idx]; 
-	    removeFromSidebar(idx); 
-	} else { 
-	    icon.setUrl(pdp.app_root + "/images/mini_triangle_selected.png");
-	    selectionList[idx] = dataArray[idx]; 
-	    addToSidebar(idx); 
-	} 
-	stnLayer.redraw()
-    }
-
     var controls = downloadForm.appendChild(getHydroStnControls(map));
 
     // Creates search box and adds it to DOM in the "Selection" fieldset.
-    var searchBox = createSearchBox('searchBox', '', [],
-				    function(event, ui) { 
-					toggleIdxSelection(ui.item.value, dataArray[ui.item.value].icon); 
-					$('#searchBox').val(''); 
-					return false; 
-				    });
+    var searchBox = createSearchBox(
+        'searchBox', '', [],
+        function(event, ui) {
+            toggleIdxSelection(ui.item.value, dataArray[ui.item.value].icon, selectionList, dataArray);
+            $('#searchBox').val('');
+            return false;
+        }
+    );
     document.getElementById('selectedStations').appendChild(searchBox);
 
     $.ajax(pdp.app_root + "/csv/routed_flow_metadatav4.csv")
@@ -76,7 +42,7 @@ $(document).ready(function() {
                 var marker = new OpenLayers.Marker(pt, icon.clone());
                 dataArray[idx].icon = marker.icon;
                 marker.events.register('mousedown', marker, function(evt) {
-                    toggleIdxSelection(idx, this.icon);
+                    toggleIdxSelection(idx, this.icon, selectionList, dataArray);
                     OpenLayers.Event.stop(evt); 
                 });
                 stnLayer.addMarker(marker);
--- pdp/static/js/hydro_stn_controls.js
+++ pdp/static/js/hydro_stn_controls.js
@@ -8,3 +8,44 @@ function getHydroStnControls(map) {
     div.appendChild(fieldset);
     return div;
 }
+
+// Add an item from the Selection sidebar
+var addToSidebar = function(idx, dataArray) {
+    var item = pdp.createDiv('stnNo' + idx, '');
+    item.textContent = dataArray[idx].StationName;
+    $('#selectedStations').append(item);
+};
+
+// Remove an item from the Selection sidebar
+var removeFromSidebar = function(idx) {
+    $('#stnNo' + idx).remove();
+};
+
+// Create a search box using jquery ui's autocomplete control.
+var createSearchBox = function(id, cssClass, data, select_callback) {
+    var frag = document.createDocumentFragment();
+    var elem = frag.appendChild(pdp.createInputElement("text", cssClass, id, id, ''));
+    $(elem).autocomplete({
+        source: data,
+        select: select_callback,
+        delay: 100,
+        minLength: 2
+    });
+    return frag;
+};
+
+// Toggle whether an index into the data array is selected.
+var toggleIdxSelection = function(idx, icon, selectionList, dataArray) {
+    var stnLayer = map.getLayersByName("Stations")[0];
+
+    if(idx in selectionList) {
+        icon.setUrl(pdp.app_root + "/images/mini_triangle.png");
+        delete selectionList[idx];
+        removeFromSidebar(idx);
+    } else {
+        icon.setUrl(pdp.app_root + "/images/mini_triangle_selected.png");
+        selectionList[idx] = dataArray[idx];
+        addToSidebar(idx, dataArray);
+    }
+    stnLayer.redraw();
+};
