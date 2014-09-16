5cc1594d43affa5b7571cceb6c740dc128bdbe7c
Basil Veerman <bveerman@uvic.ca>
2014-09-15 18:45:47 -0700

    Remove erroneous form submit action, add placeholder text w/IE8 compatibility (but not IE9?)

--- pdp/static/js/hydro_stn_controls.js
+++ pdp/static/js/hydro_stn_controls.js
@@ -6,6 +6,7 @@ function getHydroStnControls() {
     frag.appendChild(div);
 
     var form = pdp.createForm("download-form", "download-form");
+    $(form).submit(false);
     div.appendChild(form);
 
     var fieldset = pdp.createFieldset("filterset", "Selection");
@@ -14,6 +15,20 @@ function getHydroStnControls() {
     var sBox = createSearchBox("searchBox")
     fieldset.appendChild(sBox);
     frag.sBox = sBox;
+
+    $(sBox).focus(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+        }
+    }).blur(function() {
+        var input = $(this);
+        if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+        }
+    }).blur();
 
     var selection = pdp.createDiv('selectedStations', '');
     fieldset.appendChild(selection);
@@ -36,6 +51,7 @@ var removeFromSidebar = function(idx) {
 // Create a search box using jquery ui's autocomplete control.
 var createSearchBox = function(id, cssClass, data, select_callback) {
     var sbox = pdp.createInputElement("text", cssClass, id, id, '');
+    sbox.placeholder = "Station Name or ID"
     return sbox;
 };
 
