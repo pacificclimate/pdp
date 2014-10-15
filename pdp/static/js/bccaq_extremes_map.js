3ddd67c3b755cba04f2ae86bfaf4fabe6c5826fe
James Hiebert <james@hiebert.name>
2014-10-15 12:19:07 -0700

    Refactored BCCAQ Extremes page to not use hard coded color scales and to reset the full WMS parameter set every request (so that params from past requests that are no longer applicable get removed)

--- pdp/static/js/bccaq_extremes_map.js
+++ pdp/static/js/bccaq_extremes_map.js
@@ -6,55 +6,78 @@
 var current_dataset;
 
 var init_raster_map = function() {
-    function customize_wms_params(layer_name) {
+
+    function ncwms_params(layer_name) {
+        // common ncWMS parameters for *all* layers
+        var params = {
+            LAYERS: layer_name,
+            transparent: "true",
+            numcolorbands: 254,
+            version: "1.1.1",
+            SRS: "EPSG:4326",
+            service: "WMS",
+            request: "GetMap",
+            WIDTH: 512,
+            HEIGHT: 512,
+            format: 'image/png'
+        };
+
 	var varname = layer_name.split('/')[1];
 	var is_yearly = layer_name.match(/_yr_/)
 	if(is_yearly)
-	    this.params.TIME = "1950-07-02T00:00:00Z";
+	    params.TIME = "1950-07-02T00:00:00Z";
 	else
-	    this.params.TIME = "1950-07-16T00:00:00Z";
+ 	    params.TIME = "1950-07-16T00:00:00Z";
 	
 	var percent_data = { COLORSCALERANGE: "0,100", STYLES: 'boxfill/ferret', LOGSCALE: false };
 	var number_days_data = { COLORSCALERANGE: "0, 366", STYLES: 'boxfill/ferret', LOGSCALE: false};
 	var temp_data = { STYLES: 'boxfill/ferret', LOGSCALE: false };
-	var prec_data = { STYLES: 'boxfill/occam_inv', LOGSCALE: false };
+	var prec_data = { STYLES: 'boxfill/occam_inv', LOGSCALE: true };
 
 	var var_data = { 
 	    r1mmETCCDI: number_days_data,
-	    r10mmETCCDI: $.extend({ COLORSCALERANGE: "0,150"}, number_days_data),
-	    r20mmETCCDI: $.extend({ COLORSCALERANGE: "0,100"}, number_days_data),
-	    tn10pETCCDI: percent_data, 'tx10pETCCDI': percent_data, 'tn90pETCCDI': percent_data, 'tx90pETCCDI': percent_data,
-	    dtrETCCDI: $.extend({ COLORSCALERANGE: "0,25"}, temp_data),
-	    tnnETCCDI: $.extend({ COLORSCALERANGE: "-80,25"}, temp_data),
-	    tnxETCCDI: $.extend({ COLORSCALERANGE: "-50,35"}, temp_data),
-	    txnETCCDI: $.extend({ COLORSCALERANGE: "-65,45"}, temp_data),
-	    txxETCCDI: $.extend({ COLORSCALERANGE: "-45,55"}, temp_data),
-	    rx5dayETCCDI: $.extend({ COLORSCALERANGE: "0,1500"}, prec_data),
-	    rx1dayETCCDI: $.extend({ COLORSCALERANGE: "0,1000"}, prec_data),
-	    prcptotETCCDI: $.extend({ COLORSCALERANGE: "0,5000"}, prec_data),
+	    r10mmETCCDI: number_days_data,
+	    r20mmETCCDI: number_days_data,
+	    tn10pETCCDI: percent_data,
+	    tx10pETCCDI: percent_data,
+	    tn90pETCCDI: percent_data,
+	    tx90pETCCDI: percent_data,
+	    dtrETCCDI: temp_data,
+	    tnnETCCDI: temp_data,
+	    tnxETCCDI: temp_data,
+	    txnETCCDI: temp_data,
+	    txxETCCDI: temp_data,
+	    rx5dayETCCDI: prec_data,
+	    rx1dayETCCDI: prec_data,
+	    prcptotETCCDI: prec_data,
 	    gslETCCDI: number_days_data,
-	    suETCCDI: $.extend({ COLORSCALERANGE: "0,200"}, number_days_data), 
-	    trETCCDI: $.extend({ COLORSCALERANGE: "0,150"}, number_days_data),
+	    suETCCDI: number_days_data,
+	    trETCCDI: number_days_data,
 	    idETCCDI: number_days_data,
 	    fdETCCDI: number_days_data,
-	    sdiiETCCDI: $.extend({ COLORSCALERANGE: "0,50"}, prec_data),
-	    cwdETCCDI: $.extend({ COLORSCALERANGE: "0,100"}, number_days_data),
-	    altcwdETCCDI: $.extend({ COLORSCALERANGE: "0,100"}, number_days_data),
+	    sdiiETCCDI: prec_data,
+	    cwdETCCDI: number_days_data,
+	    altcwdETCCDI: number_days_data,
 	    cddETCCDI: number_days_data,
 	    altcddETCCDI: number_days_data,
-	    csdiETCCDI: $.extend({ COLORSCALERANGE: "0,150"}, number_days_data),
-	    altcsdiETCCDI: $.extend({ COLORSCALERANGE: "0,150"}, number_days_data),
+	    csdiETCCDI: number_days_data,
+	    altcsdiETCCDI: number_days_data,
 	    wsdiETCCDI: number_days_data,
 	    altwsdiETCCDI: number_days_data,
-	    r95pETCCDI: $.extend({ COLORSCALERANGE: "0,4000"}, prec_data),
-	    r99pETCCDI: $.extend({ COLORSCALERANGE: "0,4000"}, prec_data)
+	    r95pETCCDI: prec_data,
+	    r99pETCCDI: prec_data
 	};
 
-	$.extend(this.params, var_data[varname])
 	$('#map-title').html(layer_name + '<br />' + this.params.TIME);
+	$.extend(params, var_data[varname])
 
-	return true;
+	return params;
     };
+
+    function set_ncwms_params(layer_name) {
+        this.params = ncwms_params(layer_name);
+        return true;
+    }
 
     // Map Config
     var options = na4326_map_options();
@@ -76,21 +99,13 @@ var init_raster_map = function() {
         dataset: "rx1dayETCCDI_yr_BCCAQ-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_1950-2100",
         variable: "rx1dayETCCDI"
     };
-    
-    var params = {
-        LAYERS: defaults.dataset + "/" + defaults.variable,
-        transparent: "true",
-        numcolorbands: 254,
-        version: "1.1.1",
-        srs: "EPSG:4326",
-    };
 
     var datalayerName = "Climate raster";
     var ncwms =  new OpenLayers.Layer.WMS(
         datalayerName,
-		pdp.ncwms_url,
-		params,
-		{
+	pdp.ncwms_url,
+	ncwms_params(defaults.dataset + "/" + defaults.variable),
+	{
             buffer: 1,
             ratio: 1.5,
             wrapDateLine: true,
@@ -98,12 +113,12 @@ var init_raster_map = function() {
             transitionEffect: null,
             tileSize: new OpenLayers.Size(512, 512)
         }
-	);
+    );
 
-    current_dataset = params.layers;
+    current_dataset = ncwms.params.layers;
 
-    ncwms.events.register('change', ncwms, customize_wms_params);
-    ncwms.events.triggerEvent('change', defaults.dataset);
+    ncwms.events.register('change', ncwms, set_ncwms_params);
+    ncwms.events.triggerEvent('change', defaults.dataset + "/" + defaults.variable);
 
     (function(globals){
         "use strict"
