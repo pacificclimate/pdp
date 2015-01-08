12301d4e9ea8762f1d36f5f4970758abc9e599e2
James Hiebert <hiebert@uvic.ca>
2015-01-08 14:53:50 -0800

    Implemented JSLint recommendations for the remaining pdp_*.js files

--- pdp/static/js/pdp_dom_library.js
+++ pdp/static/js/pdp_dom_library.js
@@ -1,20 +1,23 @@
+/*jslint browser: true, devel: true */
+/*global $, jQuery */
+
 window.pdp = (function (my, $) {
     "use strict";
-    my.curry = function ( fn /*, ... */) {
-        var curryArgs = Array.prototype.slice.call( arguments, 1 );
-        return function( /* ... */ ) {
-            var newArgs = Array.prototype.slice.call( arguments, 0 ), mergedArgs = curryArgs.concat( newArgs );
-            return fn.apply( this, mergedArgs );
+    my.curry = function (fn) {
+        var curryArgs = Array.prototype.slice.call(arguments, 1);
+        return function () {
+            var newArgs = Array.prototype.slice.call(arguments, 0), mergedArgs = curryArgs.concat(newArgs);
+            return fn.apply(this, mergedArgs);
         };
     };
 
     my.createInputElement = function (type, cssClass, id, name, value) {
         var ie = document.createElement("input");
         ie.type = type;
-        if(typeof cssClass != "undefined") ie.className = cssClass;
-        if(typeof id != "undefined") ie.id = id;
-        if(typeof name != "undefined") ie.name = name;
-        if(typeof value != "undefined") { 
+        if (cssClass !== undefined) { ie.className = cssClass; }
+        if (id !== undefined) { ie.id = id; }
+        if (name !== undefined) { ie.name = name; }
+        if (value !== undefined) {
             ie.value = value;
             ie.defaultValue = value;
         }
@@ -23,24 +26,24 @@ window.pdp = (function (my, $) {
 
     my.createTextarea = function (id, value, readonly) {
         var ta = document.createElement("textarea");
-        if(typeof id != "undefined") ta.id = id;
-        if(typeof value != "undefined") {
+        if (id !== undefined) { ta.id = id; }
+        if (value !== undefined) {
             ta.value = ta.defaultValue = value;
         }
-        if(typeof readonly != "undefined") ta.readonly = "readonly";
+        if (readonly !== undefined) { ta.readonly = "readonly"; }
         return ta;
     };
 
     my.createDiv = function (id, className) {
         var div = document.createElement("div");
-        if(typeof id != "undefined") div.id = id;
-        if(typeof className != "undefined") div.className = className;
+        if (id !== undefined) { div.id = id; }
+        if (className !== undefined) { div.className = className; }
         return div;
     };
 
     my.createLabel = function (id, text, forId) {
         var label = document.createElement("label");
-        if(typeof id != "undefined") label.id = id;
+        if (id !== undefined) { label.id = id; }
         label.appendChild(document.createTextNode(text));
         label.htmlFor = forId;
         return label;
@@ -48,25 +51,26 @@ window.pdp = (function (my, $) {
 
     my.createLegend = function (id, text) {
         var legend = document.createElement("legend");
-        if(typeof id != "undefined") legend.id = id;
+        if (id !== undefined) { legend.id = id; }
         legend.appendChild(document.createTextNode(text));
         return legend;
     };
 
     my.createForm = function (id, name, method, action) {
         var form = document.createElement("form");
-        if(typeof id != "undefined") form.id = id;
-        if(typeof name != "undefined") form.name = name;
-        if(typeof method != "undefined") form.method = method;
-        if(typeof action != "undefined") form.action = action;
+        if (id !== undefined) { form.id = id; }
+        if (name !== undefined) { form.name = name; }
+        if (method !== undefined) { form.method = method; }
+        if (action !== undefined) { form.action = action; }
         return form;
     };
 
     my.createFieldset = function (id, label) {
         var fieldset = document.createElement("fieldset");
-        if(typeof id != "undefined") fieldset.id = id;
-        if (typeof label != "undefined")
-        fieldset.appendChild(my.createLegend(id + "-legend", label));
+        if (id !== undefined) { fieldset.id = id; }
+        if (label !== undefined) {
+            fieldset.appendChild(my.createLegend(id + "-legend", label));
+        }
         return fieldset;
     };
 
@@ -74,7 +78,7 @@ window.pdp = (function (my, $) {
         var option = document.createElement("option");
         option.appendChild(document.createTextNode(text));
         option.value = val;
-        if(typeof selected != "undefined") option.selected=selected;
+        if (selected !== undefined) { option.selected = selected; }
         return option;
     };
 
@@ -87,35 +91,35 @@ window.pdp = (function (my, $) {
 
     my.getOptionsRecursive = function (vals, defaultValue) {
         var frag = document.createDocumentFragment();
-        $.each(vals, function(idx, val) {
-        if($.isArray(val)) {
-            frag.appendChild(my.createOptgroup(idx, my.getOptionsRecursive(val[0], defaultValue)));
-        } else
-            if(typeof defaultValue != "undefined" && idx == defaultValue) {
-            frag.appendChild(my.createOption(idx, val.name, "selected"));
+        $.each(vals, function (idx, val) {
+            if ($.isArray(val)) {
+                frag.appendChild(my.createOptgroup(idx, my.getOptionsRecursive(val[0], defaultValue)));
+            } else if (defaultValue !== undefined && idx === defaultValue) {
+                frag.appendChild(my.createOption(idx, val.name, "selected"));
             } else {
-            frag.appendChild(my.createOption(idx, val.name));
+                frag.appendChild(my.createOption(idx, val.name));
             }
-        return true;
+            return true;
         });
         return frag;
     };
 
     my.createSelect = function (id, name, vals, defaultValue) {
-        var select = document.createElement("select");
+        var select, ret;
+        select = document.createElement("select");
         select.name = name;
-        if(typeof id != "undefined") select.id = id;
-        var ret = my.getOptionsRecursive(vals, defaultValue);
+        if (id !== undefined) { select.id = id; }
+        ret = my.getOptionsRecursive(vals, defaultValue);
         select.appendChild(ret);
         return select;
     };
 
     my.createLink = function (id, title, href, text, name) {
         var link = document.createElement("a");
-        if(typeof id != "undefined") link.id = id;
-        if(typeof title != "undefined") link.title = title;
-        if(typeof href != "undefined") link.href = href;
-        if(typeof name != "undefined") link.name = name;
+        if (id !== undefined) { link.id = id; }
+        if (title !== undefined) { link.title = title; }
+        if (href !== undefined) { link.href = href; }
+        if (name !== undefined) { link.name = name; }
         link.appendChild(document.createTextNode(text));
         return link;
     };
@@ -128,9 +132,9 @@ window.pdp = (function (my, $) {
     };
 
     my.createHelpItem = function (name, description) {
-        var frag = document.createDocumentFragment();
-        var dt = document.createElement("dt");
-        var dd = document.createElement("dd");
+        var frag = document.createDocumentFragment(),
+            dt = document.createElement("dt"),
+            dd = document.createElement("dd");
         dt.appendChild(document.createTextNode(name));
         dd.appendChild(document.createTextNode(description));
         frag.appendChild(dt);
@@ -139,8 +143,8 @@ window.pdp = (function (my, $) {
     };
 
     my.createHelpGroup = function (name, subtree, depth) {
-        var frag = document.createDocumentFragment();
-        var header = document.createElement("h" + depth);
+        var frag = document.createDocumentFragment(),
+            header = document.createElement("h" + depth);
         header.appendChild(document.createTextNode(name));
         frag.appendChild(header);
         frag.appendChild(subtree);
@@ -149,19 +153,19 @@ window.pdp = (function (my, $) {
 
     my.getHelpRecursive = function (vals, depth) {
         var frag = document.createDocumentFragment();
-        $.each(vals, function(idx, val) {
-        if($.isArray(val))
-            frag.appendChild(my.createHelpGroup(idx, my.getHelpRecursive(val[0]), depth + 1));
-        else
-            if(typeof val.help != "undefined")
-            frag.appendChild(my.createHelpItem(val.name, val.help));
-        return true;
+        $.each(vals, function (idx, val) {
+            if ($.isArray(val)) {
+                frag.appendChild(my.createHelpGroup(idx, my.getHelpRecursive(val[0]), depth + 1));
+            } else if (val.help !== undefined) {
+                frag.appendChild(my.createHelpItem(val.name, val.help));
+            }
+            return true;
         });
-        
+
         return frag;
     };
 
-    my.createDialog = function(div, title, width, height) {
+    my.createDialog = function (div, title, width, height) {
         $(div).dialog({
             appendTo: "#main",
             autoOpen: false,
@@ -170,29 +174,31 @@ window.pdp = (function (my, $) {
             height: height,
             modal: true,
             buttons: {
-                "Close": function() {
+                "Close": function () {
                     $(this).dialog("close");
                 }
             }
         });
-    }
+    };
 
     my.createHelp = function (helpDivId, helpData, title, width, height, helpElementType, helpCallback) {
-        if(typeof helpElementType == "undefined") helpElementType = "dl";
-        if(typeof helpCallback == "undefined") helpCallback = my.getHelpRecursive;
-        var div = my.createDiv(helpDivId);
-        var frag = document.createDocumentFragment();
-        var parent_elem = div.appendChild(document.createElement(helpElementType));
+        var div, frag, parent_elem;
+        if (helpElementType === undefined) { helpElementType = "dl"; }
+        if (helpCallback === undefined) { helpCallback = my.getHelpRecursive; }
+        div = my.createDiv(helpDivId);
+        frag = document.createDocumentFragment();
+        parent_elem = div.appendChild(document.createElement(helpElementType));
         parent_elem.appendChild(helpCallback(helpData, 2));
-	my.createDialog(div, title, width, height);
+        my.createDialog(div, title, width, height);
 
         return frag;
     };
 
     my.getTextareaLabeled = function (id, label, value, readonly) {
-        var frag = document.createDocumentFragment();
+        var frag;
+        frag = document.createDocumentFragment();
         frag.appendChild(my.createLabel(id + "-label", label, id));
-        var ta = frag.appendChild(my.createTextarea(id, value, readonly));
+        frag.appendChild(my.createTextarea(id, value, readonly));
         return frag;
     };
 
@@ -211,13 +217,13 @@ window.pdp = (function (my, $) {
     };
 
     my.getSelectorWithHelp = function (prettyName, divId, selectName, selectId, defaultValue, data, helpLinkTooltip, helpWidth, helpHeight, helpElementType, helpCallback) {
-        var div = my.getSelector(prettyName, divId, selectName, selectId, defaultValue, data);
-        var helpDivId = divId + "-help";
-        var helpLinkId = helpDivId + "-link";
+        var div = my.getSelector(prettyName, divId, selectName, selectId, defaultValue, data),
+            helpDivId = divId + "-help",
+            helpLinkId = helpDivId + "-link";
         div.appendChild(my.createHelpLink(helpLinkId, helpLinkTooltip));
         div.appendChild(my.createHelp(helpDivId, data, prettyName, helpWidth, helpHeight, helpElementType, helpCallback));
 
-        $("#" + helpLinkId, div).click(function() {
+        $("#" + helpLinkId, div).click(function () {
             $("#" + helpDivId).dialog("open");
             return false;
         });
