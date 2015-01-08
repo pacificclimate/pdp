22bdbade8cfd64324bd127bba1d9df6da778271e
James Hiebert <hiebert@uvic.ca>
2015-01-08 07:56:59 -0800

    Ran jsLint through several files and made *most* of the suggested changes

--- pdp/static/js/pdp_map.js
+++ pdp/static/js/pdp_map.js
@@ -1,78 +1,79 @@
+/*global pdp, document, $, jquery, OpenLayers, selectionBbox
+*/
+/*debug true*/
+
+"use strict";
 // Set standard map properties
 function getProjection(projnum) {
     return new OpenLayers.Projection("EPSG:" + projnum);
 }
 
-function getBC3005Bounds() { 
-    return new OpenLayers.Bounds(-236114,41654.75,2204236,1947346.25);
+function getBC3005Bounds() {
+    return new OpenLayers.Bounds(-236114, 41654.75, 2204236, 1947346.25);
 }
 
-function getBC3005Bounds_vic() { 
-    return new OpenLayers.Bounds(611014.125,251336.4375,2070975.0625,1737664.5625);
+function getBC3005Bounds_vic() {
+    return new OpenLayers.Bounds(611014.125, 251336.4375, 2070975.0625, 1737664.5625);
 }
 
 function getNA4326Bounds() {
-    return new OpenLayers.Bounds(-150,40,-50,90);
+    return new OpenLayers.Bounds(-150, 40, -50, 90);
 }
 
 function getWorld4326Bounds() {
-    return new OpenLayers.Bounds(-180,-90,180,90);
+    return new OpenLayers.Bounds(-180, -90, 180, 90);
 }
 
 function getBC3005Resolutions() {
-    return [2218.5, 1109.25, 554.625, 277.3125, 138.6562, 69.32812, 34.66406]
+    return [2218.5, 1109.25, 554.625, 277.3125, 138.6562, 69.32812, 34.66406];
 }
 
 function BC3005_map_options() {
-    bounds = getBC3005Bounds()
-
-    var options = {
-        restrictedExtent: bounds,
-        displayProjection: getProjection(4326),
-        projection: getProjection(3005),
-        units: 'Meter'
-    };
-    return options
+    var bounds = getBC3005Bounds(),
+        options = {
+            restrictedExtent: bounds,
+            displayProjection: getProjection(4326),
+            projection: getProjection(3005),
+            units: 'Meter'
+        };
+    return options;
 }
 
 function BC3005_map_options_vic() {
-    bounds = getBC3005Bounds_vic()
-
-    var options = {
-        restrictedExtent: bounds,
-        displayProjection: getProjection(4326),
-        projection: getProjection(3005),
-        units: 'Meter'
-    };
-    return options
+    var bounds = getBC3005Bounds_vic(),
+        options = {
+            restrictedExtent: bounds,
+            displayProjection: getProjection(4326),
+            projection: getProjection(3005),
+            units: 'Meter'
+        };
+    return options;
 }
 
 function na4326_map_options() {
-    bounds = getNA4326Bounds();
-    projection = getProjection(4326);
-
-    var options = {
-        restrictedExtent: bounds,
-        units: 'degrees',
-        displayProjection: projection,
-        projection: projection,
-    };
-    return options
+    var bounds = getNA4326Bounds(),
+        projection = getProjection(4326),
+        options = {
+            restrictedExtent: bounds,
+            units: 'degrees',
+            displayProjection: projection,
+            projection: projection,
+        };
+    return options;
 }
 
 function world4326_map_options() {
-    bounds = getWorld4326Bounds()
-    projection = getProjection(4326)
-
-    var options = {
-        restrictedExtent: bounds,
-        displayProjection: projection,
-        maxResolution: 0.703125,
-        numZoomLevels: 15,
-        projection: projection,
-        units: 'degrees'
-    };
-    return options
+    var bounds = getWorld4326Bounds(),
+        projection = getProjection(4326),
+        options = {
+            restrictedExtent: bounds,
+            displayProjection: projection,
+            maxResolution: 0.703125,
+            numZoomLevels: 15,
+            projection: projection,
+            units: 'degrees'
+        };
+    return options;
 }
 
 function getGSBaseLayer(gs_url, displayname, layername) {
@@ -82,10 +83,11 @@ function getGSBaseLayer(gs_url, displayname, layername) {
         {
             layers: layername,
             transparent: true
-        },{
-            isBaseLayer:true,
-            restrictedExtent: new OpenLayers.Bounds(-236114,41654.75,2204236,1947346.25),
-            maxExtent: new OpenLayers.Bounds(-236114,41654.75,2204236,1947346.25),
+        },
+        {
+            isBaseLayer: true,
+            restrictedExtent: new OpenLayers.Bounds(-236114, 41654.75, 2204236, 1947346.25),
+            maxExtent: new OpenLayers.Bounds(-236114, 41654.75, 2204236, 1947346.25),
             resolutions: [2218.5, 1109.25, 554.625, 277.3125, 138.6562, 69.32812, 34.66406],
             attribution: '© OpenStreetMap contributors'
         }
@@ -98,7 +100,8 @@ function getNaBaseLayer(wmsurl, displayname, layername, proj) {
         wmsurl,
         {
             layers: layername,
-        },{
+        },
+        {
             projection: proj,
             maxResolution: 1.40625,
             numZoomLevels: 10,
@@ -114,7 +117,7 @@ function getTileBaseLayer(tilesurl, displayname, layername, proj) {
         tilesurl + "/" + layername + "/${z}/${x}/${y}.png",
         {
             projection: proj,
-            zoomOffset:4,
+            zoomOffset: 4,
             attribution: '© OpenStreetMap contributors'
         }
     );
@@ -130,22 +133,23 @@ function getBC3005OsmBaseLayer(wmsurl, displayname, layername) {
         {
             projection: getProjection(3005),
             units: 'Meter',
-            maxExtent: new OpenLayers.Bounds(-1000000,-1000000,3000000,3000000),
+            maxExtent: new OpenLayers.Bounds(-1000000, -1000000, 3000000, 3000000),
             maxResolution: 7812.5,
             numZoomLevels: 12,
             attribution: '© OpenStreetMap contributors'
-        })
+        }
+    );
 }
 
 function getBasicControls() {
     return [
-        new OpenLayers.Control.LayerSwitcher({'ascending':false}),
+        new OpenLayers.Control.LayerSwitcher({'ascending': false}),
         new OpenLayers.Control.ScaleLine({geodesic: true}),
         new OpenLayers.Control.KeyboardDefaults(),
         new OpenLayers.Control.MousePosition({div: $('#location')[0]}),
-        new OpenLayers.Control.PanZoomBar({panIcons:true}),
+        new OpenLayers.Control.PanZoomBar({panIcons: true}),
         new OpenLayers.Control.Attribution()
-    ]
+    ];
 }
 
 function getEditingToolbar(controls) {
@@ -162,7 +166,7 @@ function getEditingToolbar(controls) {
 function getHandNav() {
     return new OpenLayers.Control.Navigation(
         {
-            handleRightClicks:true,
+            handleRightClicks: true,
             zoomBox: new OpenLayers.Control.ZoomBox(),
             enabled: true,
             selected: true
@@ -177,16 +181,16 @@ function getPolygonLayer() {
             'geometryType': OpenLayers.Geometry.Polygon
         }
     );
-} 
+}
 
 function getBoxLayer(name) {
-    name = typeof name !== 'undefined' ? name : "Box selection";
+    name = (name !== undefined ? name : "Box selection");
     var boxLayer = new OpenLayers.Layer.Vector(name);
     // Allow only one rectangle at a time
     boxLayer.events.register(
         'beforefeatureadded',
         boxLayer,
-        function(evt) {
+        function (evt) {
             this.removeAllFeatures();
             boxLayer.ncbounds = new OpenLayers.Bounds();
         }
@@ -194,17 +198,18 @@ function getBoxLayer(name) {
     boxLayer.events.register(
         'featureadded',
         boxLayer,
-        function(evt) {
+        function (evt) {
             selectionBbox = this.features[0].geometry.getBounds();
         }
     );
-    return boxLayer
+    return boxLayer;
 }
 
-function getPolyEditor(poly) {    
+function getPolyEditor(poly) {
     return new OpenLayers.Control.DrawFeature(
         poly,
-        OpenLayers.Handler.Polygon, {
+        OpenLayers.Handler.Polygon,
+        {
             'displayClass': 'olControlDrawFeaturePolygon'
         }
     );
@@ -213,39 +218,45 @@ function getPolyEditor(poly) {
 function getBoxEditor(box) {
     return new OpenLayers.Control.DrawFeature(
         box,
-        OpenLayers.Handler.RegularPolygon, {
+        OpenLayers.Handler.RegularPolygon,
+        {
             handlerOptions: {
-                sides: 4, irregular: true
+                sides: 4,
+                irregular: true
             },
             displayClass: 'olControlDrawFeaturePolygon'
         }
     );
 }
 
-var getOpacitySlider = function(layer) {
+var getOpacitySlider = function (layer) {
+
+    var sliderContainer,
+        sliderTitle,
+        sliderElement;
 
     // Container
-    var sliderContainer = document.createElement('div');
+    sliderContainer = document.createElement('div');
     sliderContainer.className = "opacitySliderContainer";
     sliderContainer.style.padding = "0.5em";
-    
+
     // Title
-    var sliderTitle = document.createElement('div');
+    sliderTitle = document.createElement('div');
     sliderTitle.className = "opacitySliderTitle";
     sliderTitle.innerHTML = 'Climate Layer Opacity';
     sliderTitle.setAttribute("unselectable", "on");
     sliderTitle.className += " unselectable";
     sliderTitle.style.display = "block";
     sliderTitle.style.marginBottom = "0.5em";
-    sliderTitle.style.padding = "0 2em"
-    sliderTitle.style.fontWeight = "bold"
+    sliderTitle.style.padding = "0 2em";
+    sliderTitle.style.fontWeight = "bold";
 
     sliderContainer.appendChild(sliderTitle);
 
     // Slider Element
-    var sliderElement = document.createElement('div');
+    sliderElement = document.createElement('div');
     sliderElement.style.position = "relative";
-    sliderElement.style.marginBottom = "0.5em"
+    sliderElement.style.marginBottom = "0.5em";
     sliderElement.className = "opacitySliderElement";
     sliderContainer.appendChild(sliderElement);
 
@@ -255,7 +266,7 @@ var getOpacitySlider = function(layer) {
         range: "min",
         min: 0,
         value: 70,
-        slide: function(e, ui) {
+        slide: function (e, ui) {
             layer.setOpacity(ui.value / 100);
         }
     });
@@ -264,13 +275,14 @@ var getOpacitySlider = function(layer) {
 };
 
 function addLoadingIcon(layer) {
-    $("#map-wrapper").append('<div id="loading" class="invisible"><center>\
-<img src="' + pdp.app_root + '/images/loading.gif" alt="Layer loading animation" /><p>Loading...</p>\
-</center></div>');
+    $("#map-wrapper").append('<div id="loading" class="invisible"><center>\n' +
+        '<img src="' + pdp.app_root + '/images/loading.gif" alt="Layer loading animation" /><p>Loading...</p>\n' +
+        '</center></div>'
+         );
     layer.events.register('loadstart',
                           $("#loading"),
-                          function(evt) {this.removeClass("invisible");});
+                          function (evt) {this.removeClass("invisible"); });
     layer.events.register('loadend',
                           $("#loading"),
-                          function(evt) {this.addClass("invisible");});
+                          function (evt) {this.addClass("invisible"); });
 }
--- pdp/static/js/pdp_raster_map.js
+++ pdp/static/js/pdp_raster_map.js
@@ -1,13 +1,19 @@
-var cfTime = function(units, sDate) {
+/*global pdp, document, $, getNCWMSLayerCapabilities, catalog, getNcwmsLayerId, ddsToTimeIndex, dasToUnitsSince, setTimeAvailable
+*/
+/*debug true*/
+
+"use strict";
+
+var CfTime = function (units, sDate) {
     this.units = units;
     this.sDate = sDate;
 };
-cfTime.prototype.setMaxTimeByIndex = function(index) {
+CfTime.prototype.setMaxTimeByIndex = function (index) {
     this.maxIndex =  index;
     this.eDate = this.toDate(index);
     return this.eDate;
 };
-cfTime.prototype.toDate = function(index) {
+CfTime.prototype.toDate = function (index) {
     if (index === undefined) {
         return this.sDate;
     }
@@ -17,64 +23,71 @@ cfTime.prototype.toDate = function(index) {
         return d;
     }
 };
-cfTime.prototype.toIndex = function(d) {
+CfTime.prototype.toIndex = function (d) {
     if (d < this.sDate || (this.eDate && this.eDate < d)) {
         return;
     }
 
     if (this.units === "days") {
-        var msPerDay = 1000 * 60 * 60 * 24;
-        var msDiff = d.getTime() - this.sDate.getTime();
-        var days = msDiff / msPerDay;
+        var msPerDay = 1000 * 60 * 60 * 24,
+            msDiff = d.getTime() - this.sDate.getTime(),
+            days = msDiff / msPerDay;
         return Math.floor(days);
     }
 };
 
-var processNcwmsLayerMetadata = function(ncwms_layer) {
+var processNcwmsLayerMetadata = function (ncwms_layer) {
+
+    var layerUrl, maxTimeReq, unitsSinceReq;
+
     getNCWMSLayerCapabilities(ncwms_layer);
 
-    var layerUrl = catalog[getNcwmsLayerId(ncwms_layer)];
+    layerUrl = catalog[getNcwmsLayerId(ncwms_layer)];
     // Request time variables
-    var maxTimeReq = $.ajax({
+    maxTimeReq = $.ajax({
         url: (layerUrl + ".dds?time").replace("/data/", "/catalog/")
     });
 
-    var unitsSinceReq = $.ajax({
+    unitsSinceReq = $.ajax({
         url: (layerUrl + ".das").replace("/data/", "/catalog/")
     });
 
     // Process times when both returned
-    $.when(maxTimeReq, unitsSinceReq).done (function(maxTime, unitsSince) {
-        var maxTimeIndex = ddsToTimeIndex(maxTime[0]);
+    $.when(maxTimeReq, unitsSinceReq).done(function (maxTime, unitsSince) {
+
+        var maxTimeIndex, units, startDate, layerTime;
+
+        maxTimeIndex = ddsToTimeIndex(maxTime[0]);
         unitsSince = dasToUnitsSince(unitsSince[0]);
-        var units = unitsSince[0];
-        var startDate = unitsSince[1];
-        var layerTime = new cfTime(units, startDate);
+        units = unitsSince[0];
+        startDate = unitsSince[1];
+        layerTime = new CfTime(units, startDate);
         layerTime.setMaxTimeByIndex(maxTimeIndex);
         ncwms_layer.times = layerTime; // Future access through ncwmslayer?
         setTimeAvailable(layerTime.sDate, layerTime.eDate);
     });
 };
 
-var getNcwmsLayerId = function(ncwms_layer) {
+var getNcwmsLayerId = function (ncwms_layer) {
     return ncwms_layer.params.LAYERS.split("/")[0];
 };
 
-var ddsToTimeIndex = function(data) {
+var ddsToTimeIndex = function (data) {
     return parseInt(data.match(/\[time.*]/g)[0].match(/\d+/)[0], 10);
 };
 
-var dasToUnitsSince = function(data) {
-    var s = data.match(/time \{[\s\S]*?\}/gm)[0];
-    var reg = /\"(.*?) since (.*?)\"/g;
-    var m = reg.exec(s);
-    var units = m[1];
-    var dateString = m[2];
-    
+var dasToUnitsSince = function (data) {
+    var i,
+        s = data.match(/time \{[\s\S]*?\}/gm)[0],
+        reg = /\"(.*?) since (.*?)\"/g,
+        m = reg.exec(s),
+        units = m[1],
+        dateString = m[2];
+
     reg = /(\d{4})-(\d{2}|\d)-(\d{2}|\d)( |T)(\d{2}|\d):(\d{2}|\d):(\d{2}|\d)/g;
     m = reg.exec(dateString);
-    if(m) {
-        for (var i = 0; i < m.length; i++) { m[i] = +m[i]; }
+    if (m) {
+        for (i = 0; i < m.length; i++) { m[i] = +m[i]; }
         var sDate = new Date(m[1], --m[2], m[3], m[5], m[6], m[7], 0); // Account for 0 based month index in js
         return [units, sDate];
     }
@@ -88,7 +101,7 @@ var dasToUnitsSince = function(data) {
     return undefined;
 };
 
-var getNCWMSLayerCapabilities = function(ncwms_layer) {
+var getNCWMSLayerCapabilities = function (ncwms_layer) {
 
     // FIXME: this .ajax logic doesn't really work in all cases
     // What we really want is the fail() handler to _resolve_ the status,
@@ -105,15 +118,15 @@ var getNCWMSLayerCapabilities = function(ncwms_layer) {
 	    data: params,
 	   })
 	.fail(handle_ie8_xml)
-	.always(function(response, status, jqXHR) {
+	.always(function (response, status, jqXHR) {
 	    window.ncwmsCapabilities = $(jqXHR.responseXML);
 	});
 }
 
-var setTimeAvailable = function(begin, end) {
+var setTimeAvailable = function (begin, end) {
     //TODO: only present times available in ncwms capabilities for this layer
 
-    $.each([".datepickerstart", ".datepickerend"], function(idx, val) {
+    $.each([".datepickerstart", ".datepickerend"], function (idx, val) {
         $(val).datepicker("option", "minDate", begin);
         $(val).datepicker("option", "maxDate", end);
     });
@@ -122,7 +135,7 @@ var setTimeAvailable = function(begin, end) {
     $(".datepickerend").datepicker("setDate", end);
 };
 
-var intersection = function(b1, b2) {
+var intersection = function (b1, b2) {
     // take the intersection of two OpenLayers.Bounds
     return new OpenLayers.Bounds(
         Math.max(b1.left, b2.left),
@@ -132,14 +145,14 @@ var intersection = function(b1, b2) {
     );
 };
 
-var getRasterNativeProj = function(capabilities, layer_name) {
+var getRasterNativeProj = function (capabilities, layer_name) {
     // Return the dataset native projection as an OL.Projection object
     var bbox = capabilities.find('Layer > Name:contains("' + layer_name + '")').parent().find('BoundingBox')[0];
     var srs = bbox.attributes.getNamedItem('SRS');
     return new OpenLayers.Projection(srs.value);
 };
 
-var getRasterBbox = function(capabilities, layer_name) {
+var getRasterBbox = function (capabilities, layer_name) {
     // The WMS layer doesn't seem to have the bbox of the _data_ available, which I would like to have
     // Pull the geographic bounding box out of the appropriate element
     var bbox = capabilities.find('Layer > Name:contains("' + layer_name + '")').parent().find('LatLonBoundingBox')[0];
@@ -149,7 +162,7 @@ var getRasterBbox = function(capabilities, layer_name) {
     return real_bounds;
 };
 
-var getTimeSelected = function(ncwms_layer) {
+var getTimeSelected = function (ncwms_layer) {
     var base = new Date($(".datepickerstart").datepicker("option", "minDate"));
     var t0 = $(".datepickerstart").datepicker("getDate");
     var tn = $(".datepickerend").datepicker("getDate");
@@ -162,7 +175,7 @@ var getTimeSelected = function(ncwms_layer) {
 var rasterBBoxToIndicies = function (map, layer, bnds, extent_proj, extension, callback) {
     var indexBounds = new OpenLayers.Bounds();
 
-    var responder = function(data, status, response) {
+    var responder = function (data, status, response) {
         var xmldoc;
         if (response.responseXML) {
             xmldoc = response.responseXML;
@@ -181,7 +194,7 @@ var rasterBBoxToIndicies = function (map, layer, bnds, extent_proj, extension, c
         }
     };
 
-    var requestIndex = function(x, y) {
+    var requestIndex = function (x, y) {
         var params = {
             REQUEST: "GetFeatureInfo",
             BBOX: map.getExtent().toBBOX(),
