7c3fc0d3fdea63d43890948ed2722c963a2d402b
James Hiebert <hiebert@uvic.ca>
2015-01-09 09:27:52 -0800

    Implemented JSLint recommendations for the remaining vic_gen1_*.js files

--- pdp/static/js/vic_gen1_app.js
+++ pdp/static/js/vic_gen1_app.js
@@ -1,6 +1,14 @@
-// Globals ensemble_name, current_dataset, ncwmsCapabilities
+/*jslint browser: true, devel: true */
+/*global $, jQuery, OpenLayers, pdp, getCatalog, download, init_vic_map, processNcwmsLayerMetadata, getVICControls, getRasterDownloadOptions*/
 
-$(document).ready(function() {
+"use strict";
+
+// globals
+var catalog;
+
+$(document).ready(function () {
+    var map, loginButton, ncwmsLayer, selectionLayer, type;
+
     map = init_vic_map();
     loginButton = pdp.init_login('login-div');
     pdp.checkLogin(loginButton);
@@ -8,29 +16,29 @@ $(document).ready(function() {
     ncwmsLayer = map.getClimateLayer();
     selectionLayer = map.getSelectionLayer();
 
-    getCatalog(function (data) { catalog = data;
-				 processNcwmsLayerMetadata(ncwmsLayer);
-			       });
+    getCatalog(function (data) {
+        catalog = data;
+        processNcwmsLayerMetadata(ncwmsLayer);
+    });
 
-    var selector = document.getElementById("pdp-controls").appendChild(getVICControls(pdp.ensemble_name));
-    var downloader = document.getElementById("pdp-controls").appendChild(getRasterDownloadOptions(true));
+    document.getElementById("pdp-controls").appendChild(getVICControls(pdp.ensemble_name));
+    document.getElementById("pdp-controls").appendChild(getRasterDownloadOptions(true));
 
     function callDownload() {
         download(type, map, selectionLayer, ncwmsLayer, 'data');
-    };
+    }
     function showDownloadLink() {
         download(type, map, selectionLayer, ncwmsLayer, 'link');
-    };
+    }
     function callDownloadMetadata() {
         download('das', map, selectionLayer, ncwmsLayer, 'metadata');
-    };
-    var type;
+    }
 
-    $("#download-timeseries").click(function(){
-        type = $('select[name="data-format"]').val()
+    $("#download-timeseries").click(function () {
+        type = $('select[name="data-format"]').val();
         callDownload();
     });
-    $("#permalink").click(function(){
+    $("#permalink").click(function () {
         type = $('select[name="data-format"]').val();
         showDownloadLink();
     });
