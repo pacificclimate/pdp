c9aebcb1521f5983cf7a38fbac993412341ae764
James Hiebert <hiebert@uvic.ca>
2015-01-09 12:58:57 -0800

    Wrote an event handler for bc_prism's download link that inserts the climatology_bounds variable when appropriate

--- pdp/static/js/prism_demo_app.js
+++ pdp/static/js/prism_demo_app.js
@@ -13,17 +13,29 @@ $(document).ready(function() {
     ncwmsLayer = map.getClimateLayer();
     selectionLayer = map.getSelectionLayer();
 
+    // Ensure that climatology_bounds are included in non-aig downloads
+    function setBoundsInUrlTemplate () {
+        if (this.ext ==== 'aig') {
+            this.url_template = '{dl_url}.{ext}?climatology_bounds,{var_}[{trange}][{yrange}][{xrange}]&';
+        } else {
+            '{dl_url}.{ext}?{var_}[{trange}][{yrange}][{xrange}]&';
+        }
+    }
+
     var dlLink = new RasterDownloadLink($('#download-timeseries'), ncwmsLayer, undefined, 'nc', '', '0:13', ':', ':');
-    dlLink.url_template = '{dl_url}.{ext}?climatology_bounds,{var_}[{trange}][{yrange}][{xrange}]&'; // FIXME: this won't work for aig
     $('#data-format-selector').change(function(evt) {dlLink.onExtensionChange($(this).val())});
     ncwmsLayer.events.register('change', dlLink, dlLink.onLayerChange);
+    ncwmsLayer.events.register('change', dlLink, setBoundsInUrlTemplate);
     selectionLayer.events.register('featureadded', dlLink, dlLink.onBoxChange);
     dlLink.register($('#download-timeseries'), function(node) {node.attr('href', dlLink.getUrl())});
+    dlLInk.trigger()
 
     var mdLink = new RasterDownloadLink($('#download-metadata'), ncwmsLayer, undefined, 'das', '', '0:13', ':', ':');
+    mdLink.url_template = '{dl_url}.das?climatology_bounds,{var_}[{trange}][{yrange}][{xrange}]&';
     ncwmsLayer.events.register('change', mdLink, mdLink.onLayerChange);
     selectionLayer.events.register('featureadded', mdLink, mdLink.onBoxChange);
     mdLink.register($('#download-metadata'), function(node) {node.attr('href', mdLink.getUrl())});
+    mdLink.trigger()
 
     // FIXME: This needs to have error handling and this is horrible
     getCatalog(function (data) {catalog = dlLink.catalog = mdLink.catalog = data;});
