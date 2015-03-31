f4fe9ffc4ec55429b5a641c56f87a1fe8ca6988f
James Hiebert <hiebert@uvic.ca>
2015-03-30 19:18:24 -0700

    Modified all startup scripts for new constructed app location

--- scripts/eventlet_serve.py
+++ scripts/eventlet_serve.py
@@ -4,15 +4,15 @@ from logging import basicConfig, DEBUG
 import eventlet
 from eventlet import wsgi
 
-from pdp import main
+from pdp.wsgi import dev_server
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser(description='Start a development pdp:main Flask instance')
+    parser = ArgumentParser(description='Start a development pdp.wsgi:dev_server Flask instance')
     parser.add_argument('-p', '--port', type=int, required=True,
                         help='Indicate the port on which to bind the application')
     args = parser.parse_args()
 
     basicConfig(format='%(levelname)s:%(name)s:%(asctime)s %(message)s', stream=sys.stdout, level=DEBUG)
     
-    wsgi.server(eventlet.listen(('0.0.0.0', args.port)), main(), debug=True)
+    wsgi.server(eventlet.listen(('0.0.0.0', args.port)), dev_server, debug=True)
