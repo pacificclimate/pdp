e369f81767e2ae75b4aab1fdbf9b4e153b9bd86d
James Hiebert <james@hiebert.name>
2018-02-23 15:19:52 -0800

    Fix PEP8 violations in main.py

--- pdp/main.py
+++ pdp/main.py
@@ -29,7 +29,8 @@ from portals.hydro_stn import data_server as hydro_stn_data_server
 
 import portals.bcsd_downscale_canada as bcsd_canada
 from portals.bcsd_downscale_canada import portal as bcsd_canada_portal
-from portals.bcsd_downscale_canada import data_server as bcsd_canada_data_server
+from portals.bcsd_downscale_canada import data_server as \
+    bcsd_canada_data_server
 
 import portals.bccaq_extremes as bccaq_extremes
 from portals.bccaq_extremes import portal as bccaq_extremes_portal
@@ -41,12 +42,8 @@ from portals.vic_gen1 import data_server as vic_gen1_data_server
 
 import portals.gridded_observations as gridded_observations
 from portals.gridded_observations import portal as gridded_observations_portal
-from portals.gridded_observations import data_server as gridded_observations_data_server
-
-#global_config = get_config()
-#dsn = global_config['dsn']
-#pcds_dsn = global_config['pcds_dsn']
-#atexit.register(clean_session_dir, global_config['session_dir'], global_config['clean_session_dir'])
+from portals.gridded_observations import data_server as \
+    gridded_observations_data_server
 
 
 def initialize_frontend(global_config, use_auth=False, use_analytics=False):
@@ -64,7 +61,8 @@ def initialize_frontend(global_config, use_auth=False, use_analytics=False):
         ('^/check_auth_app/?$', check_auth),
         ('^/{}/.*$'.format(pcds.url_base), pcds_portal(global_config)),
         ('^/pcds_map/.*$', pcds_portal(global_config)),  # legacy url support
-        ('^/{}/.*$'.format(hydro_stn.url_base), hydro_stn_portal(global_config)),
+        ('^/{}/.*$'.format(hydro_stn.url_base),
+         hydro_stn_portal(global_config)),
         ('^/{}/.*$'.format(bc_prism.url_base), bc_prism_portal(global_config)),
         ('^/{}/.*$'.format(vic_gen1.url_base), vic_gen1_portal(global_config)),
         ('^/{}/.*$'.format(gridded_observations.url_base),
@@ -81,8 +79,9 @@ def initialize_frontend(global_config, use_auth=False, use_analytics=False):
     if use_auth:
         wsgi_app = SessionMiddleware(
             wsgi_app, auto=1, data_dir=global_config['session_dir'])
-        atexit.register(
-            clean_session_dir, global_config['session_dir'], global_config['clean_session_dir'])
+        atexit.register(clean_session_dir,
+                        global_config['session_dir'],
+                        global_config['clean_session_dir'])
     return ErrorMiddleware(wsgi_app)
 
 
@@ -97,9 +96,11 @@ def initialize_backend(global_config, use_auth=False, use_analytics=False):
         ('^/{}/.*$'.format(vic_gen1.url_base),
          vic_gen1_data_server(global_config, vic_gen1.ensemble_name)),
         ('^/{}/.*$'.format(gridded_observations.url_base),
-         gridded_observations_data_server(global_config, gridded_observations.ensemble_name)),
+         gridded_observations_data_server(global_config,
+                                          gridded_observations.ensemble_name)),
         ('^/{}/.*$'.format(bccaq_extremes.url_base),
-         bccaq_extremes_data_server(global_config, bccaq_extremes.ensemble_name)),
+         bccaq_extremes_data_server(global_config,
+                                    bccaq_extremes.ensemble_name)),
         ('^/{}/.*$'.format(hydro_stn.url_base),
          hydro_stn_data_server(global_config)),
         ('^/{}/.*$'.format(pcds.url_base), pcds_data_server(global_config))
@@ -109,14 +110,18 @@ def initialize_backend(global_config, use_auth=False, use_analytics=False):
     if use_auth:
         wsgi_app = SessionMiddleware(
             wsgi_app, auto=1, data_dir=global_config['session_dir'])
-        atexit.register(
-            clean_session_dir, global_config['session_dir'], global_config['clean_session_dir'])
+        atexit.register(clean_session_dir,
+                        global_config['session_dir'],
+                        global_config['clean_session_dir'])
     return ErrorMiddleware(wsgi_app)
 
 
 def initialize_dev_server(global_config, use_auth=False, use_analytics=False):
     '''Development server
     '''
-    return DispatcherMiddleware(initialize_frontend(global_config, use_auth, use_analytics), {
-        '/data': initialize_backend(global_config, use_auth, use_analytics)
-    })
+    return DispatcherMiddleware(
+        initialize_frontend(global_config, use_auth, use_analytics),
+        {
+            '/data': initialize_backend(global_config, use_auth, use_analytics)
+        }
+    )
