a0888dc36d6206844b067e155c275f816157a970
Rod Glover <rglover@uvic.ca>
2019-04-29 12:37:47 -0700

    Make ie8.js a little more user-friendly (no functional or usage change)

--- pdp/static/js/ie8.js
+++ pdp/static/js/ie8.js
@@ -1,19 +1,15 @@
-'use strict';
-
-window.shittyIE = (!Object.keys && !Array.prototype.IndexOf);
-
 if (!Object.keys) {
-  Object.keys = function(obj) {
-    var keys = [];
+    Object.keys = function (obj) {
+        var keys = [];
 
-    for (var i in obj) {
-      if (obj.hasOwnProperty(i)) {
-        keys.push(i);
-      }
-    }
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                keys.push(i);
+            }
+        }
 
-    return keys;
-  };
+        return keys;
+    };
 }
 
 
@@ -22,16 +18,16 @@ if (!Object.keys) {
 // Add ECMA262-5 method binding if not supported natively
 //
 if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
-            return function() {
+    Function.prototype.bind = function (owner) {
+        var that = this;
+        if (arguments.length <= 1) {
+            return function () {
                 return that.apply(owner, arguments);
             };
         } else {
-            var args= Array.prototype.slice.call(arguments, 1);
-            return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
+            var args = Array.prototype.slice.call(arguments, 1);
+            return function () {
+                return that.apply(owner, arguments.length === 0 ? args : args.concat(Array.prototype.slice.call(arguments)));
             };
         }
     };
@@ -40,7 +36,7 @@ if (!('bind' in Function.prototype)) {
 // Add ECMA262-5 string trim if not supported natively
 //
 if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
+    String.prototype.trim = function () {
         return this.replace(/^\s+/, '').replace(/\s+$/, '');
     };
 }
@@ -48,63 +44,63 @@ if (!('trim' in String.prototype)) {
 // Add ECMA262-5 Array methods if not supported natively
 //
 if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (i in this && this[i]===find)
+    Array.prototype.indexOf = function (find, i /*opt*/) {
+        if (i === undefined) i = 0;
+        if (i < 0) i += this.length;
+        if (i < 0) i = 0;
+        for (var n = this.length; i < n; i++)
+            if (i in this && this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('lastIndexOf' in Array.prototype)) {
-    Array.prototype.lastIndexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= this.length-1;
-        if (i<0) i+= this.length;
-        if (i>this.length-1) i= this.length-1;
-        for (i++; i-->0;) /* i++ because from-argument is sadly inclusive */
-            if (i in this && this[i]===find)
+    Array.prototype.lastIndexOf = function (find, i /*opt*/) {
+        if (i === undefined) i = this.length - 1;
+        if (i < 0) i += this.length;
+        if (i > this.length - 1) i = this.length - 1;
+        for (i++; i-- > 0;) /* i++ because from-argument is sadly inclusive */
+            if (i in this && this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.forEach = function (action, that /*opt*/) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this)
                 action.call(that, this[i], i, this);
     };
 }
 if (!('map' in Array.prototype)) {
-    Array.prototype.map= function(mapper, that /*opt*/) {
-        var other= new Array(this.length);
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.map = function (mapper, that /*opt*/) {
+        var other = new Array(this.length);
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this)
-                other[i]= mapper.call(that, this[i], i, this);
+                other[i] = mapper.call(that, this[i], i, this);
         return other;
     };
 }
 if (!('filter' in Array.prototype)) {
-    Array.prototype.filter= function(filter, that /*opt*/) {
-        var other= [], v;
-        for (var i=0, n= this.length; i<n; i++)
-            if (i in this && filter.call(that, v= this[i], i, this))
+    Array.prototype.filter = function (filter, that /*opt*/) {
+        var other = [], v;
+        for (var i = 0, n = this.length; i < n; i++)
+            if (i in this && filter.call(that, v = this[i], i, this))
                 other.push(v);
         return other;
     };
 }
 if (!('every' in Array.prototype)) {
-    Array.prototype.every= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.every = function (tester, that /*opt*/) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this && !tester.call(that, this[i], i, this))
                 return false;
         return true;
     };
 }
 if (!('some' in Array.prototype)) {
-    Array.prototype.some= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.some = function (tester, that /*opt*/) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (i in this && tester.call(that, this[i], i, this))
                 return true;
         return false;
@@ -113,21 +109,34 @@ if (!('some' in Array.prototype)) {
 
 // End of from http://stackoverflow.com/a/2790686 //
 
-function handle_ie8_xml(response, status, jqXHR) {
-    if (status === 'parsererror') { // We must be in IE8 with broken XML parsing
-	var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-	xmlDoc.async = false;
-	xmlDoc.validateOnParse = false;
-	xmlDoc.resolveExternals = false;
-	xmlDoc.loadXML(response.responseText);
 
-	if (xmlDoc.parseError.errorCode != 0) {
-	    var myErr = xmlDoc.parseError;
-	    console.log("You have error " + myErr.reason);
-	}
 
-	jqXHR.responseXML = xmlDoc;
-    } else {
-	alert("There was an unhandleable problem fetching some metadata for the current layer. Unfortunately this prevents some of the controls from functioning properly. Please try a page reload and if the problem persists, please file a bug report.");
+(function (window) {
+
+    'use strict';
+
+    window.shittyIE = (!Object.keys && !Array.prototype.IndexOf);
+
+    window.handle_ie8_xml = function (response, status, jqXHR) {
+        if (status === 'parsererror') { // We must be in IE8 with broken XML parsing
+            var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+            xmlDoc.async = false;
+            xmlDoc.validateOnParse = false;
+            xmlDoc.resolveExternals = false;
+            xmlDoc.loadXML(response.responseText);
+
+            if (xmlDoc.parseError.errorCode != 0) {
+                var myErr = xmlDoc.parseError;
+                console.log("You have error " + myErr.reason);
+            }
+
+            jqXHR.responseXML = xmlDoc;
+        } else {
+            alert("There was an unhandleable problem fetching some metadata " +
+                "for the current layer. Unfortunately this prevents some of " +
+                "the controls from functioning properly. Please try a page " +
+                "reload and if the problem persists, please file a bug report.")
+            ;
+        }
     }
-};
+})(window);
