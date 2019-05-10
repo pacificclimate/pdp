be0a2a77f3a168403e6102282ddf904bf27c5d61
Rod Glover <rglover@uvic.ca>
2019-05-10 15:14:52 -0700

    Use jest-environment-jsdom-fourteen

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
         // Get rid of Error: Not implemented: HTMLCanvasElement.prototype.getContext
         // See https://github.com/hustcc/jest-canvas-mock/issues/2#issuecomment-468600415
         './node_modules/jest-canvas-mock/lib/index.js',
-    ]
+    ],
+    "testEnvironment": "jest-environment-jsdom-fourteen"
 };
