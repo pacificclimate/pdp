d9a9dfb5dd79d5b61e37b4a2e5390f5abaac2da1
Rod Glover <rglover@uvic.ca>
2019-05-24 11:42:19 -0700

    Add tests for canada_ex_app and related modules

---
+++ pdp/static/js/__test__/canada_ex_app.test.js
@@ -0,0 +1,73 @@
+test('test', function () {});
+
+require('./app-test-helpers').initializeGlobals();
+
+require('./globals-helpers').importGlobals([
+    // External packages
+    { module: 'js/ie8.js' },  // execute only
+    { module: 'js/jquery-1.10.2.js', name: ['$', 'jQuery'] },
+    { module: 'js/jquery-ui-1.10.2.custom.js' }, // execute only
+    { module: 'js/multiaccordion.js' },  // jQuery plugin
+    { module: 'js/zebra.js' },  // execute only
+    { include: 'js/OL/OpenLayers-2.13.1.debug.js' },
+    { include: 'js/proj4js-compressed.js' },
+    { module: 'js/lodash.core.js', name: ['_', 'lodash'] },
+
+    // Local packages
+    { module: 'js/condExport', name: 'condExport' },
+    { module: 'js/classes.js', name: 'classes' },
+    { module: 'js/calendars.js', name: 'calendars' },
+    { module: 'js/data-services.js', name: 'dataServices' },
+
+    { module: 'js/pdp_dom_library.js', spread: false },
+    { module: 'js/pdp_controls.js', spread: true },
+    { module: 'js/pdp_download.js', spread: true },
+    { module: 'js/pdp_filters.js', spread: true },
+    { module: 'js/pdp_map.js', spread: true },
+    { module: 'js/pdp_raster_map.js', spread: true },
+    { module: 'js/pdp_vector_map.js', spread: true },
+
+    // Statistically Downscaled GCM Scenarios apps
+    { module: 'js/canada_ex_map.js', spread: true },
+    { module: 'js/canada_ex_app.js', name: 'canada_ex_app' },
+], '../..');
+
+var dateFilterTests = require('./date-filter-tests');
+
+
+jest.mock('../../js/data-services');
+var dataServices = require('../../js/data-services');
+
+
+var mockHelpers = require('./mock-helpers');
+mockHelpers.mock$ajax({ log: true, throw: true });
+mockHelpers.mockOLXMLHttpRequest({ log: true, throw: true });
+
+
+var beforeCalendar = calendars['gregorian'];
+var beforeUnits = 'days';
+var beforeDefaultCfTimeSystem = new calendars.CfTimeSystem(
+    beforeUnits,
+    new calendars.CalendarDatetime(beforeCalendar, 1870, 1, 1),
+    Math.floor((2100 - 1870 + 1) * 365.2425)
+);
+
+var afterCalendar = calendars['365_day'];
+var afterUnits = 'days';
+var afterDefaultCfTimeSystem = new calendars.CfTimeSystem(
+    afterUnits,
+    new calendars.CalendarDatetime(afterCalendar, 1950, 1, 1),
+    Math.floor((2100 - 1950 + 1) * 365)
+);
+
+dateFilterTests(canada_ex_app, {
+    defaultCfTimeSystem: afterDefaultCfTimeSystem,
+    defaultStartDate: {
+        before: calendars.CfDatetime.fromDatetime(beforeDefaultCfTimeSystem, 1950, 1, 1),
+        after: calendars.CfDatetime.fromDatetime(afterDefaultCfTimeSystem, 1950, 1, 1)
+    },
+    defaultEndDate: {
+        before: beforeDefaultCfTimeSystem.todayAsCfDatetime(),
+        after: afterDefaultCfTimeSystem.todayAsCfDatetime()
+    }
+});
---
+++ pdp/static/js/__test__/pdp_controls.test.js
@@ -0,0 +1,71 @@
+require('./globals-helpers').importGlobals([
+    { module: 'js/jquery-1.10.2.js', name: ['$', 'jQuery'] },
+    { module: 'js/lodash.core.js', name: ['_', 'lodash'] },
+    { module: 'js/condExport.js', name: 'condExport' },
+    { module: 'js/classes.js', name: 'classes' },
+    { module: 'js/calendars.js', name: 'calendars' },
+    { module: 'js/pdp_controls.js', spread: true },
+], '../..');
+
+
+describe('RasterDownloadLink', function() {
+    document.body.innerHTML =
+        '<div>' +
+            '<a id="download-timeseries" href="#"/>' +
+        '</div>';
+
+    var ncwmsLayer = {
+        events: {
+            register: function() {}
+        }
+    };
+
+    describe('mock app setup', function() {
+        var dlLink;
+        beforeEach(function() {
+            dlLink = new RasterDownloadLink(
+                $('#download-timeseries'),
+                ncwmsLayer,
+                undefined,
+                'nc',
+                'tasmax',
+                't0:t1',
+                'y0:y1',
+                'x0:x1'
+            );
+        });
+
+        afterEach(function() {
+            dlLink = null;
+        });
+
+        it('has the supplied initial time range', function() {
+            expect(dlLink.trange).toBe('t0:t1');
+        });
+
+        it('generates the expected download URL', function() {
+           expect(dlLink.getUrl()).toBe('.nc?tasmax[t0:t1][y0:y1][x0:x1]&')
+        });
+
+        it('calls back a registered function with supplied context', function() {
+            var context = { foo: 'bar' };
+            var callback = jest.fn();
+            dlLink.register(context, callback);
+            dlLink.trigger();
+            expect(callback.mock.calls.length).toBe(1);
+            expect(callback.mock.calls[0][0]).toBe(context);
+        });
+
+        it('produces the expected effect with code copied from canada_ex_app',
+            function() {
+                dlLink.register($('#download-timeseries'), function(node) {
+                        node.attr('href', dlLink.getUrl());
+                    }
+                );
+                dlLink.trigger();
+                expect($('#download-timeseries').attr('href'))
+                    .toEqual('.nc?tasmax[t0:t1][y0:y1][x0:x1]&')
+            }
+        );
+    });
+});
