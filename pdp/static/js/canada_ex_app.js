0490c46b695bd31a1b3adf561d84d65252e0e8de
Rod Glover <rglover@uvic.ca>
2019-05-24 11:58:13 -0700

    Modify canada_ex_app and related modules

--- pdp/static/js/canada_ex_app.js
+++ pdp/static/js/canada_ex_app.js
@@ -20,118 +20,123 @@
  * They have different starting datasets. Everything else is the same.
  */
 
-"use strict";
+(function ($) {
+    "use strict";
 
-$(document).ready(function () {
-    var map, ncwmsLayer, selectionLayer, catalogUrl, catalog_request, catalog,
-        dlLink, mdLink, capabilities_request, ncwms_capabilities;
+    function canada_ex_app() {
+        var map, ncwmsLayer, selectionLayer, catalogUrl, catalog_request, catalog,
+            dlLink, mdLink, capabilities_request, ncwms_capabilities;
 
-    //check whether we're on the archive portal: is "archive" in the url?
-    const archivePortal = $(location).attr('href').indexOf("archive") != -1;
+        //check whether we're on the archive portal: is "archive" in the url?
+        const archivePortal = $(location).attr('href').indexOf("archive") != -1;
 
-    //the two portals have different initial maps
-    if (archivePortal) {
-      //the old dataset
-      map = init_raster_map({
-        variable: "tasmax",
-        dataset: "pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231",
-        timestamp: "2000-01-01"
-      });
-    } else {
-      map = init_raster_map({
-        variable: "tasmax",
-        dataset: "tasmax_day_BCCAQv2_CanESM2_historical-rcp26_r1i1p1_19500101-21001231_Canada",
-        timestamp: "2000-01-01T12:00:00.00Z"
-      });
-    }
-
-    ncwmsLayer = map.getClimateLayer();
-    selectionLayer = map.getSelectionLayer();
-
-    catalogUrl = "../catalog/catalog.json";
-    catalog_request = $.ajax(catalogUrl, {dataType: "json"});
-
-    capabilities_request = getNCWMSLayerCapabilities(ncwmsLayer);
-
-    document.getElementById("pdp-controls").appendChild(getRasterControls(pdp.ensemble_name));
-    document.getElementById("pdp-controls").appendChild(getRasterDownloadOptions(true));
-
-    //UI elements vary slightly based on whether this is the archive or new portal.
-    if(archivePortal) {
-      //archive portal. link to new portal, add archive disclaimer.
-      const portalLink = pdp.createLink("portal-link",
-          undefined,
-          pdp.app_root + "/downscaled_gcms/map/",
-          "Main Downscaled GCMS Portal");
-      document.getElementById("topnav").appendChild(portalLink);
-      document.getElementById("pdp-controls").appendChild(getArchiveDisclaimer());
-    } else {
-      //new portal. link to old one
-      const portalLink = pdp.createLink("portal-link",
-          undefined,
-          pdp.app_root + "/downscaled_gcms_archive/map/",
-          "Archive Downscaled GCMS Portal");
-      document.getElementById("topnav").appendChild(portalLink);
-    }
-
-    // Data Download Link
-    dlLink = new RasterDownloadLink($('#download-timeseries'), ncwmsLayer, undefined, 'nc', 'tasmax', '0:55152', '0:510', '0:1068');
-    $('#data-format-selector').change(
-        function (evt) {
-            dlLink.onExtensionChange($(this).val());
+        //the two portals have different initial maps
+        if (archivePortal) {
+            //the old dataset
+            map = init_raster_map({
+                variable: "tasmax",
+                dataset: "pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231",
+                timestamp: "2000-01-01"
+            });
+        } else {
+            map = init_raster_map({
+                variable: "tasmax",
+                dataset: "tasmax_day_BCCAQv2_CanESM2_historical-rcp26_r1i1p1_19500101-21001231_Canada",
+                timestamp: "2000-01-01T12:00:00.00Z"
+            });
         }
-    );
 
-    ncwmsLayer.events.register('change', dlLink, function () {
-        processNcwmsLayerMetadata(ncwmsLayer, catalog);
-        capabilities_request = getNCWMSLayerCapabilities(ncwmsLayer);
-        capabilities_request.done(function(data) {
-            ncwms_capabilities = data;
-            if (selectionLayer.features.length > 0) {
-                dlLink.onBoxChange({feature: selectionLayer.features[0]}, ncwms_capabilities);
+        ncwmsLayer = map.getClimateLayer();
+        selectionLayer = map.getSelectionLayer();
+
+        catalog_request = dataServices.getCatalog();
+
+        capabilities_request = dataServices.getNCWMSLayerCapabilities(ncwmsLayer);
+
+        document.getElementById("pdp-controls").appendChild(getRasterControls(pdp.ensemble_name));
+        document.getElementById("pdp-controls").appendChild(getRasterDownloadOptions(true));
+
+        //UI elements vary slightly based on whether this is the archive or new portal.
+        if(archivePortal) {
+            //archive portal. link to new portal, add archive disclaimer.
+            const portalLink = pdp.createLink("portal-link",
+                undefined,
+                pdp.app_root + "/downscaled_gcms/map/",
+                "Main Downscaled GCMS Portal");
+            document.getElementById("topnav").appendChild(portalLink);
+            document.getElementById("pdp-controls").appendChild(getArchiveDisclaimer());
+        } else {
+            //new portal. link to old one
+            const portalLink = pdp.createLink("portal-link",
+                undefined,
+                pdp.app_root + "/downscaled_gcms_archive/map/",
+                "Archive Downscaled GCMS Portal");
+            document.getElementById("topnav").appendChild(portalLink);
+        }
+
+        // Data Download Link
+        dlLink = new RasterDownloadLink($('#download-timeseries'), ncwmsLayer, undefined, 'nc', 'tasmax', '0:55152', '0:510', '0:1068');
+        $('#data-format-selector').change(
+            function (evt) {
+                dlLink.onExtensionChange($(this).val());
             }
-        });
-    });
-    ncwmsLayer.events.register('change', dlLink, dlLink.onLayerChange);
+        );
 
-    selectionLayer.events.register('featureadded', dlLink, function (selection){
-        capabilities_request.done(function(data) {
+        ncwmsLayer.events.register('change', dlLink, function () {
+            processNcwmsLayerMetadata(ncwmsLayer, catalog);
+            capabilities_request = dataServices.getNCWMSLayerCapabilities(ncwmsLayer);
+            capabilities_request.done(function(data) {
+                ncwms_capabilities = data;
+                if (selectionLayer.features.length > 0) {
+                    dlLink.onBoxChange({feature: selectionLayer.features[0]}, ncwms_capabilities);
+                }
+            });
+        });
+        ncwmsLayer.events.register('change', dlLink, dlLink.onLayerChange);
+
+        selectionLayer.events.register('featureadded', dlLink, function (selection){
+            capabilities_request.done(function(data) {
+                ncwms_capabilities = data;
+                dlLink.onBoxChange(selection, data);
+            });
+        });
+
+        dlLink.register($('#download-timeseries'), function (node) {
+                node.attr('href', dlLink.getUrl());
+            }
+        );
+        dlLink.trigger();
+
+        // Metadata/Attributes Download Link
+        mdLink = new MetadataDownloadLink($('#download-metadata'), ncwmsLayer, undefined);
+        ncwmsLayer.events.register('change', mdLink, mdLink.onLayerChange);
+        mdLink.register($('#download-metadata'), function (node) {
+                node.attr('href', mdLink.getUrl());
+            }
+        );
+        mdLink.trigger();
+
+        // Date picker event for both links
+        $("[class^='datepicker']").change(
+            function (evt) {
+                dlLink.onTimeChange();
+            }
+        );
+
+        capabilities_request.done(function (data) {
             ncwms_capabilities = data;
-            dlLink.onBoxChange(selection, data);
         });
-    });
+        catalog_request.done(function (data) {
+            catalog = dlLink.catalog = mdLink.catalog = data;
+            processNcwmsLayerMetadata(ncwmsLayer, catalog);
+            // Set the data URL as soon as it is available
+            dlLink.onLayerChange();
+            mdLink.onLayerChange();
+        });
 
-    dlLink.register($('#download-timeseries'), function (node) {
-        node.attr('href', dlLink.getUrl());
     }
-                   );
-    dlLink.trigger();
 
-    // Metadata/Attributes Download Link
-    mdLink = new MetadataDownloadLink($('#download-metadata'), ncwmsLayer, undefined);
-    ncwmsLayer.events.register('change', mdLink, mdLink.onLayerChange);
-    mdLink.register($('#download-metadata'), function (node) {
-        node.attr('href', mdLink.getUrl());
-    }
-                   );
-    mdLink.trigger();
+    condExport(module, canada_ex_app, 'canada_ex_app');
 
-    // Date picker event for both links
-    $("[class^='datepicker']").change(
-        function (evt) {
-            dlLink.onTimeChange();
-        }
-    );
-
-    capabilities_request.done(function (data) {
-        ncwms_capabilities = data;
-    });
-    catalog_request.done(function (data) {
-        catalog = dlLink.catalog = mdLink.catalog = data;
-        processNcwmsLayerMetadata(ncwmsLayer, catalog);
-        // Set the data URL as soon as it is available
-        dlLink.onLayerChange();
-        mdLink.onLayerChange();
-    });
-
-});
+    $(document).ready(canada_ex_app);
+})(jQuery);
