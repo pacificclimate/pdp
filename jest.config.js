7a7bc8c9c58a3e1b4f872aebc427eb6688fd6daa
Rod Glover <rglover@uvic.ca>
2019-05-30 17:09:09 -0700

    Update jest.config.js to fix XMLSerializer errors

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  setupFiles: [
-    // Fix HTMLCanvasElement.prototype.getContext, and canvas errors.
-    './node_modules/jest-canvas-mock/lib/index.js'
-  ]
+    name: 'pdp-frontend-tests',
+    setupFiles: [
+        // Get rid of Error: Not implemented: HTMLCanvasElement.prototype.getContext
+        // See https://github.com/hustcc/jest-canvas-mock/issues/2#issuecomment-468600415
+        './node_modules/jest-canvas-mock/lib/index.js',
+    ],
+    testEnvironment: "jest-environment-jsdom-fourteen"
 };
