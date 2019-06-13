95bc5330a029c7fe1104f9ff2e1b9a42ac70aca3
Rod Glover <rglover@uvic.ca>
2019-06-13 16:45:01 -0700

    Add vic_gen1_app tests

---
+++ pdp/static/js/__test__/vic_gen1_app.test.js
@@ -0,0 +1,75 @@
+test('test', function () {});
+
+require('./app-test-helpers').initializeGlobals();
+
+require('./globals-helpers').importGlobals([
+    // External packages
+    { module: 'js/ie8.js' },  // execute only
+    { module: 'js/jquery-1.10.2.js', name: ['$', 'jQuery'] },
+    { module: 'js/jquery-ui-1.10.2.custom.js' }, // execute only
+    { module: 'js/multiaccordion.js' },  // jQuery plugin
+    { module: 'js/zebra.js' },  // execute only
+    { include: 'js/OL/OpenLayers-2.13.1.debug.js' },
+    { include: 'js/proj4js-compressed.js' },
+    { module: 'js/lodash.custom.js', name: ['_', 'lodash'] },
+
+    // Local packages
+    { module: 'js/condExport', name: 'condExport' },
+    { module: 'js/classes.js', name: 'classes' },
+    { module: 'js/calendars.js', name: 'calendars' },
+    // Note: mocking!
+    { module: 'js/__mocks__/data-services/vic_gen1_app.js', name: 'dataServices' },
+
+    { module: 'js/pdp_dom_library.js', spread: false },
+    { module: 'js/pdp_controls.js', spread: true },
+    { module: 'js/pdp_download.js', spread: true },
+    { module: 'js/pdp_filters.js', spread: true },
+    { module: 'js/pdp_map.js', spread: true },
+    { module: 'js/pdp_raster_map.js', spread: true },
+    { module: 'js/pdp_vector_map.js', spread: true },
+
+    // Gridded Observations app
+    { module: 'js/vic_gen1_map.js', spread: true },
+    { module: 'js/vic_gen1_controls.js', spread: true },
+    { module: 'js/vic_gen1_app.js', name: 'vic_gen1_app' },
+
+], '../..');
+
+var dateFilterTests = require('./date-filter-tests');
+
+
+var mockHelpers = require('./mock-helpers');
+mockHelpers.mock$ajax({ log: true, throw: true });
+mockHelpers.mockOLXMLHttpRequest({ log: true, throw: true });
+
+
+var beforeCalendar = calendars['gregorian'];
+var beforeUnits = 'days';
+var beforeCfTimeSystem = new calendars.CfTimeSystem(
+    beforeUnits,
+    new calendars.CalendarDatetime(beforeCalendar, 1870, 1, 1),
+    Math.floor((2100 - 1870 + 1) * 365.2425)
+);
+
+var afterCalendar = calendars['standard'];
+var afterUnits = 'days';
+var afterCfTimeSystem = new calendars.CfTimeSystem(
+    afterUnits,
+    new calendars.CalendarDatetime(afterCalendar, 1950, 1, 1),
+    54787
+);
+
+dateFilterTests(vic_gen1_app, {
+    cfTimeSystem: {
+        before: beforeCfTimeSystem,
+        after: afterCfTimeSystem,
+    },
+    defaultStartDate: {
+        before: beforeCfTimeSystem.firstCfDatetime(),
+        after: afterCfTimeSystem.firstCfDatetime()
+    },
+    defaultEndDate: {
+        before: beforeCfTimeSystem.lastCfDatetime(),
+        after: afterCfTimeSystem.lastCfDatetime()
+    }
+});
