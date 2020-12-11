f3665812014ca5ac6341be3849784e190824e535
rod-glover <rod.glover.ca@gmail.com>
2020-12-11 09:12:20 -0800

    Change dataset selector WIP

--- pdp/portals/bc_prism.py
+++ pdp/portals/bc_prism.py
@@ -2,10 +2,8 @@
 the 1971-2000 and 1981-2010 climatologies and monthly climate data for
 800 meter resolution PRISM dataset for BC
 '''
-
 from pdp.portals import make_raster_frontend, data_server
 from pdp_util.ensemble_members import EnsembleMemberLister
-import re
 
 
 __all__ = ['url_base', 'mk_frontend', 'mk_backend']
@@ -16,22 +14,47 @@ url_base = '/bc_prism'
 title = 'High-Resolution PRISM Data'
 
 
+def resolution_name(time_resolution):
+    return time_resolution.capitalize()
+
+
 class PrismEnsembleLister(EnsembleMemberLister):
-    def describe_time_set(self, unique_id):
-        monthly = unique_id.find("mon") != -1
-        climatology = unique_id.find("Clim") != -1
-        date_finder = re.match(r'.*_(\d+)-(\d+).*', unique_id)
+    # def describe_time_set(self, unique_id):
+    #     # Holy shit, are you kidding me? They are extracting metadata from
+    #     # the unique_id, not from the database metadata. This needs to change.
+    #     monthly = unique_id.find("mon") != -1
+    #     climatology = unique_id.find("Clim") != -1
+    #     date_finder = re.match(r'.*_(\d+)-(\d+).*', unique_id)
+    #
+    #     return "{} {} {}-{}".format("Monthly " if monthly else "",
+    #                                 "Climatological Averages " if climatology
+    #                                 else "Timeseries ",
+    #                                 date_finder.group(1)[0:4],
+    #                                 date_finder.group(2)[0:4])
 
-        return "{} {} {}-{}".format("Monthly " if monthly else "",
-                                    "Climatological Averages " if climatology
-                                    else "Timeseries ",
-                                    date_finder.group(1)[0:4],
-                                    date_finder.group(2)[0:4])
+    # Otherwise, however, this looks OK.
 
+    # `list_stuff` is what the endpoint calls; see pdp_util.ensemble_members
+    # Somewhere along the line, its results are turned into a nested dict,
+    # with the key levels, from shallowest to deepest, corresponding to the
+    # yielded tuple elements from 0 through the second-last. The last element
+    # is the value of the deepest key.
     def list_stuff(self, ensemble):
         for dfv in ensemble.data_file_variables:
-            yield self.describe_time_set(dfv.file.unique_id),\
-                dfv.netcdf_variable_name, dfv.file.unique_id.replace('+', '-')
+            df = dfv.file
+            if dfv.is_multi_year_mean:
+                timeset = df.timeset
+                middle_year = timeset.start_date.year
+                descriptors = (
+                    "{}-{}".format(middle_year - 15, middle_year + 15),
+                    resolution_name(timeset.time_resolution)
+                )
+            else:
+                descriptors = ("Monthly Timeseries",)
+            yield \
+                (dfv.netcdf_variable_name,) \
+                + descriptors \
+                + (df.unique_id.replace('+', '-'),)
 
 
 def mk_frontend(config):
