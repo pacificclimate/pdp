09789ad9f7ad2539102904a1887e05966e0a3aaa
rod-glover <rod.glover.ca@gmail.com>
2021-04-27 11:36:21 -0700

    handle descending arrays in utils.nearestIndex

--- pdp/static/js/__test__/utils.test.js
+++ pdp/static/js/__test__/utils.test.js
@@ -19,42 +19,116 @@ function range (start, end, step) {
 
 
 describe('binarySearch', function() {
-  const a1 = range(0, 10, 0.1);
+  const a1 = range(0, 10, 0.1);  // ascending: 0, 0.1, ... 9.9
+  const n1 = a1.length;
+  const a2 = range(10, 0, -0.1);  // descending: 10, 9.9, ... 0.1
+  const n2 = a2.length;
 
   it.each([
-    [a1, 0, 0, a1.length-1, 0],
-    [a1, 2.01, 0, a1.length-1, 20],
-    [a1, 2.11, 0, a1.length-1, 21],
-    [a1, 2.91, 0, a1.length-1, 29],
-    [a1, 3.19, 0, a1.length-1, 31],
+    [a1, 2.01, 0, n1-1, 20],
+    [a1, 2.11, 0, n1-1, 21],
+    [a1, 2.91, 0, n1-1, 29],
+    [a1, 3.19, 0, n1-1, 31],
+    // some edge-ish cases
+    [a1, a1[0], 0, n1-1, 0],
+    [a1, a1[1], 0, n1-1, 1],
+    [a1, a1[10], 0, n1-1, 10],
+    [a1, a1[98], 0, n1-1, 98],
+
+    [a2, 9.99, 0, n2-1, 0],
+    [a2, 9.91, 0, n2-1, 0],
+    [a2, 0.11, 0, n2-1, n2-1 - 1],
+    [a2, 2.01, 0, n2-1, n2-1 - 20],
+    // some edge-ish cases
+    [a2, a2[0], 0, n2-1, 0],
+    [a2, a2[1], 0, n2-1, 1],
+    [a2, a2[10], 0, n2-1, 10],
+    [a2, a2[98], 0, n2-1, 98],
   ])(
     '%#',
     function (a, v, i, j, expected) {
-      const result = binarySearch(a, v, i, j);
-      expect(result).toBe(expected);
+      // Validity conditions for invoking binarySearch
+      const n = a.length;
+      expect(n).toBeGreaterThan(1);
+      expect(0).toBeLessThanOrEqual(i);
+      expect(i).toBeLessThan(j);
+      expect(j).toBeLessThan(n);
+
+      const asc = a[0] < a[1];
+      const k = binarySearch(a, asc, v, i, j);
+      
+      expect(i).toBeLessThanOrEqual(k);
+      expect(k).toBeLessThan(j);
+      if (asc) {
+        expect(a[k]).toBeLessThanOrEqual(v);
+        expect(v).toBeLessThan(a[k+1]);
+      } else {
+        expect(a[k]).toBeGreaterThanOrEqual(v);
+        expect(v).toBeGreaterThan(a[k+1]);
+      }
+      expect(k).toBe(expected);
     }
   )
 });
 
 
 describe('nearestIndex', function() {
-  const a1 = range(0, 10, 0.1);
+  const a1 = range(0, 10, 0.1);  // ascending: 0, 0.1, ... 9.9
+  const a2 = range(10, 0, -0.1);  // descending: 10, 9.9, ... 0.1
 
   it.each([
-    [a1, -17, 0],
-    [a1, 0, 0],
-    [a1, 0.2, 2],
-    [a1, 2.11, 21],
-    [a1, 2.91, 29],
-    [a1, 2.96, 30],
-    [a1, 3.11, 31],
-    [a1, 3.19, 32],
-    [a1, 42, 99],
+    [a1, -17],
+    [a1, 0],
+    [a1, 0.2],
+    [a1, 2.11],
+    [a1, 2.91],
+    [a1, 2.96],
+    [a1, 3.11],
+    [a1, 3.19],
+    [a1, 42],
+
+    [a2, -17],
+    [a2, 0],
+    [a2, 0.21],
+    [a2, 2.11],
+    [a2, 2.91],
+    [a2, 2.96],
+    [a2, 3.11],
+    [a2, 3.19],
+    [a2, 42],
   ])(
     '%#',
-    function (a, v, expected) {
-      const result = nearestIndex(a, v);
-      expect(result).toBe(expected);
+    function (a, v) {
+      const k = nearestIndex(a, v);
+
+      const n = a.length;
+
+      // Check edge cases
+      const asc = a[0] < a[1];
+      if (asc) {
+        if (v < a[0]) {
+          expect(k).toBe(0)
+        } else if (v >= a[-1]) {
+          expect(k).toBe(n-1);
+        }
+      } else {
+        if (v >= a[0]) {
+          expect(k).toBe(0)
+        } else if (v <= a[-1]) {
+          expect(k).toBe(n-1);
+        }
+      }
+
+      // Check that `a[k]` is closest to `v` of all `a`
+      const diff = a[k] - v;
+      if (k > 0) {
+        // console.log(`v = ${v}, k = ${k}; a[${k-1}]=${a[k-1]}, a[${k}]=${a[k]}`)
+        expect(Math.abs(diff)).toBeLessThan(Math.abs(a[k-1] - v));
+      }
+      if (k < n - 1) {
+        // console.log(`v = ${v}, k = ${k}; a[${k}]=${a[k]}, a[${k+1}]=${a[k+1]}`)
+        expect(Math.abs(diff)).toBeLessThan(Math.abs(a[k + 1] - v));
+      }
     }
   )
 });
--- pdp/static/js/utils.js
+++ pdp/static/js/utils.js
@@ -2,15 +2,17 @@
 
 (function () {
 
-  function binarySearch(a, v, i, j) {
-    // Given an array `a` in ascending order, and a value `v`,
-    // return the index `k` such that `a[k] <= v < a[k+1]` and `i <= k < j`.
+  function binarySearch(a, asc, v, i, j) {
+    // Given a sorted array `a`, value `v`, and indices 0 <= `i` < `j` < `n`,
+    // where `n = a.length > 1`, such that
+    //    `a[i] <= v < a[j]`. if `a` in ascending order, or
+    //    `a[i] >= v > a[j]`. if `a` in descending order,
+    // return the index `k` such that `i <= k < j`
+    //    `a[k] <= v < a[k+1]` if `a` in ascending order, or
+    //    `a[k] >= v > a[k+1]` if `a` in descending order.
+    // Argument `asc` is a boolean indicating ascending order.
     // This is a utility function for constructing searches of `a`.
-
-    // Conditions:
-    // n = len(a) > 1
-    // 0 <= i < j < n
-    // a[i] <= v < a[j]
+    // It will fail if any of the conditions above are violated.
 
     const f = 0.5;
     const d = j - i;
@@ -18,26 +20,30 @@
       return i;
     }
     const k = Math.floor(i + f * d);
-    return v < a[k] ? binarySearch(a, v, i, k) : binarySearch(a, v, k, j);
+    return (asc ? v < a[k] : v > a[k]) ?
+      binarySearch(a, asc, v, i, k) :
+      binarySearch(a, asc, v, k, j);
   }
 
 
   function nearestIndex(a, v) {
-    // Given an array `a` in ascending order, and a value `v`, return the index
-    // `k` such that `a[k] <= v < a[k+1]` if `a[0] <= v < a[n-1]`, and
-    // `0` or `n-1` if v is below or above the range of `a`, respectively,
+    // Given an array `a` and a value `v`, return the index
+    // `k` such that `abs(a[k] - v)` is minimized for all `0 <= k < n`.
     // where `n = a.length`.
 
+    const asc = a[1] > a[0];
+
     // Edge cases
-    if (v <= a[0]) {
+    if (asc ? v <= a[0] : v >= a[0]) {
       return 0;
     }
-    if (v >= a[-1]) {
+    if (asc ? v >= a[-1] : v <= a[-1]) {
       return a.length - 1;
     }
 
-    const k = binarySearch(a, v, 0, a.length-1);
-    return v <= (a[k] + a[k+1]) / 2 ? k : k + 1;
+    const k = binarySearch(a, asc, v, 0, a.length-1);
+    const mid = (a[k] + a[k+1]) / 2;
+    return (asc ? v <= mid : v >= mid) ? k : k + 1;
   }
 
 
