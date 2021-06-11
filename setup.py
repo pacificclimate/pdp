8c4468b05a246ed773602f85ff876f3015a53e77
rod-glover <rod.glover.ca@gmail.com>
2021-06-11 10:53:30 -0700

    Remove version spec from pdp-util in setup.py

--- setup.py
+++ setup.py
@@ -12,18 +12,19 @@ except ImportError:
     warn("Could not import sphinx. You won't be able to build the docs")
 
 
-__version__ = '3.0.4'
+__version__ = "3.0.4"
 
 
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['-v', '--tb=no', 'tests']
+        self.test_args = ["-v", "--tb=no", "tests"]
         self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
@@ -32,7 +33,8 @@ def recursive_list(pkg_dir, basedir):
     def find():
         for dirname, dirnames, filenames in os.walk(basedir):
             for filename in filenames:
-                yield os.path.join(dirname, filename).replace(pkg_dir, '', 1)
+                yield os.path.join(dirname, filename).replace(pkg_dir, "", 1)
+
     return [x for x in find()]
 
 
@@ -43,7 +45,7 @@ def get_commitish():
         branch = repo.active_branch.name
     except TypeError:
         branch = "detached.HEAD"
-    safe_branch = re.sub('[-/_]', '.', branch)
+    safe_branch = re.sub("[-/_]", ".", branch)
     return "{}.{}".format(safe_branch, repo.git.rev_parse(sha, short=6))
 
 
@@ -52,51 +54,52 @@ __revision__ = get_commitish()
 setup(
     name="pdp",
     description="PCIC's Data Portal (pdp): the server software to run the "
-                "entire web application",
+    "entire web application",
     keywords="opendap dods dap open data science climate meteorology "
-             "downscaling modelling",
-    packages=['pdp', 'pdp.portals'],
+    "downscaling modelling",
+    packages=["pdp", "pdp.portals"],
     version="%s+%s" % (__version__, __revision__),
     url="http://www.pacificclimate.org/",
     author="James Hiebert",
     author_email="hiebert@uvic.ca",
-    install_requires=['flask',
-                      'beaker',
-                      'genshi',
-                      'static',
-                      'pdp-util ~=1.2',
-                      'pydap.handlers.hdf5 >=0.5',
-                      'pydap.responses.netcdf >=0.5',
-                      'pydap.responses.xls',
-                      'pydap.responses.aaigrid >=0.5',
-                      'pydap.handlers.sql',
-                      'ga-wsgi-client',
-                      'slimit',
-                      'netCDF4<1.5.6'],
-    tests_require=['webob',
-                   'pytest',
-                   'xlrd',
-                   'pillow',
-                   'numpy',
-                   'BeautifulSoup4'],
-    scripts=['scripts/rast_serve.py'],
-    package_dir={'pdp': 'pdp'},
-    package_data={'pdp': ['templates/*.html',
-                          'resources/hydro_stn_archive.yaml',
-                          'resources/hydro_stn_cmip5.yaml'] +
-                  recursive_list('pdp/', 'pdp/static') +
-                  recursive_list('pdp/', 'pdp/docs/html')},
-    cmdclass={
-        'test': PyTest,
-        'build_sphinx': BuildDoc
+    install_requires=[
+        "flask",
+        "beaker",
+        "genshi",
+        "static",
+        "pdp-util",
+        "pydap.handlers.hdf5 >=0.5",
+        "pydap.responses.netcdf >=0.5",
+        "pydap.responses.xls",
+        "pydap.responses.aaigrid >=0.5",
+        "pydap.handlers.sql",
+        "ga-wsgi-client",
+        "slimit",
+        "netCDF4<1.5.6",
+    ],
+    tests_require=[
+        "webob",
+        "pytest",
+        "xlrd",
+        "pillow",
+        "numpy",
+        "BeautifulSoup4",
+    ],
+    scripts=["scripts/rast_serve.py"],
+    package_dir={"pdp": "pdp"},
+    package_data={
+        "pdp": [
+            "templates/*.html",
+            "resources/hydro_stn_archive.yaml",
+            "resources/hydro_stn_cmip5.yaml",
+        ]
+        + recursive_list("pdp/", "pdp/static")
+        + recursive_list("pdp/", "pdp/docs/html")
     },
-    command_options={
-        'build_sphinx': {
-            'build_dir': ('setup.py', 'pdp/docs')
-        }
-    },
+    cmdclass={"test": PyTest, "build_sphinx": BuildDoc},
+    command_options={"build_sphinx": {"build_dir": ("setup.py", "pdp/docs")}},
     zip_safe=False,
-    classifiers='''Development Status :: 5 - Production/Stable
+    classifiers="""Development Status :: 5 - Production/Stable
 Environment :: Console
 nvironment :: Web Environment
 Framework :: Flask
@@ -113,5 +116,7 @@ Topic :: Internet :: WWW/HTTP :: WSGI :: Application
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Atmospheric Science
 Topic :: Scientific/Engineering :: GIS
-Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+Topic :: Software Development :: Libraries :: Python Modules""".split(
+        "\n"
+    ),
 )
