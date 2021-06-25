da4fd3916e76c94cbb4f095a9826a190d53108b4
rod-glover <rod.glover.ca@gmail.com>
2021-06-25 12:51:32 -0700

    Simplify local-pytest

--- docker/local-pytest/up-backend.sh
+++ docker/local-pytest/up-backend.sh
@@ -17,7 +17,7 @@ docker run -it \
   -e "GUNICORN_PROXY_PROTOCOL=True" \
   -e "GUNICORN_TIMEOUT=86400" \
   -e "APP_MODULE=pdp.wsgi:backend" \
-  -v $(pwd):/opt/dockeragent/codebase \
+  -v $(pwd):/codebase \
   -v /storage/data:/storage/data:ro \
   --name pdp-local-pytest_backend \
   pcic/pdp-local-pytest
