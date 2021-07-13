67930571cf5be770a7ff6ea904dd717bd99ccddc
rod-glover <rod.glover.ca@gmail.com>
2021-07-13 16:01:59 -0700

    Remove old local infrastructure

--- docker/local-pytest/down-backend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker stop pdp-local-pytest_backend
-docker rm pdp-local-pytest_backend
--- docker/local-pytest/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-# Install PDP from local codebase
-pip install -i https://pypi.pacificclimate.org/simple \
-  -r requirements.txt -r test_requirements.txt -r deploy_requirements.txt
-pip install -i https://pypi.pacificclimate.org/simple -e .
-
-/bin/bash
