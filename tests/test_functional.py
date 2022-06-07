bd18f5e52790029b6e967c692cd56a825302b512
James Hiebert <james@hiebert.name>
2022-06-07 14:28:20 -0700

    Adds test case showing Zip64 failure

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -456,14 +456,17 @@ def test_aaigrid_response(pcic_data_portal, url):
 @pytest.mark.slow
 @pytest.mark.crmpdb
 @pytest.mark.bulk_data
-@pytest.mark.parametrize(
-    'layers',
-    [0, 1, 100, pytest.param(38000, marks=pytest.mark.veryslow)]
-)
+@pytest.mark.parametrize(('layers', 'format'), (
+    (0, 'aig'),
+    (1, 'aig'),
+    (100, 'aig'),
+    (pytest.param(38000, marks=pytest.mark.veryslow)], 'aig'),
+    (pytest.param(38000, marks=pytest.mark.veryslow)], 'xlsx')
+))
 def test_aaigrid_response_layers(pcic_data_portal, layers):
     url = '/data/hydro_model_archive/pr+tasmin+tasmax+wind_day_HadCM_A1B_'\
-          'run1_19500101-21001231.nc.aig?pr[0:{}][119:120]'\
-          '[242:243]&'.format(layers)
+          'run1_19500101-21001231.nc.{}?pr[0:{}][119:120]'\
+          '[242:243]&'.format(format, layers)
     req = Request.blank(url)
     resp = req.get_response(pcic_data_portal)
 
