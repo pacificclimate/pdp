9546fc2ae8440619b5b538c98fc419b7de0dd70c
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2022-09-29 11:07:55 -0700

    Remove /docs/ from test_static_full

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -32,8 +32,7 @@ def test_static(url):
 @pytest.mark.local_only
 @pytest.mark.crmpdb
 @pytest.mark.parametrize('url', [
-    '/js/crmp_map.js', '/css/main.css', '/images/banner.png', '/docs/',
-    '/robots.txt'
+    '/js/crmp_map.js', '/css/main.css', '/images/banner.png', '/robots.txt'
 ])
 def test_static_full(pcic_data_portal, url):
     req = Request.blank(url)
