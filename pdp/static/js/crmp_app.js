e56c6641e5c6883d62b156c70871da6b6ffd0eca
rod-glover <rod.glover.ca@gmail.com>
2022-10-25 15:09:06 -0700

    Revert "Amended PCDS portal deprecation notice"
    This reverts commit 48e34b6bf86b309e39d17836f781856a24aab23a.

--- pdp/static/js/crmp_app.js
+++ pdp/static/js/crmp_app.js
@@ -11,9 +11,13 @@
             <div style="text-align: center">
                 <span style="font-weight: bold; color: orangered">
                     Deprecation Notice</span>: 
-                This portal will be replaced and superseded by the 
-                    PCDS - Met Data Portal
-                on Oct 15, 2022.
+                This portal has been superseded by the 
+                <a href="https://pacificclimate.org/data/met-data-portal-pcds">
+                    Met Data Portal - PCDS</a>,
+                and will be discontinued on Nov 30, 2022.
+                For more information see the
+                <a href="https://pacificclimate.org/data/bc-station-data">
+                    BC Station Data page</a>.
             </div>
         `)
         $('#header').append(deprecationNotice);
