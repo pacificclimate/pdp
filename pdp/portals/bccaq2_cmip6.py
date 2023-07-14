a7cc52f9bd887434329fa9e3f75caa0ba5c43ec7
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2023-07-13 17:18:09 -0700

    Add PCIC12 headings to CanDCS-U6 page

--- pdp/portals/bccaq2_cmip6.py
+++ pdp/portals/bccaq2_cmip6.py
@@ -18,19 +18,34 @@ title = 'Canadian Downscaled Climate Scenarios - Univariate (CMIP6): CanDCS-U6'
 
 class CMIP6EnsembleLister(EnsembleMemberLister):
     def list_stuff(self, ensemble):
-        def format_scenario(scenario):
+        def format_scenario(scenario, pcic12=False):
             '''takes a scenario of the form "historical,ssp126" and
             formats it to be "Historical, SSP1-2.6" or similar. 
             unmatchable scenario strings are returned unchanged.'''
             parsed = re.match(r"^historical,ssp(\d)(\d)(\d)$", scenario)
             if parsed:
-                return "Historical, SSP{}-{}.{}".format(parsed.group(1), 
+                scenario = "Historical, SSP{}-{}.{}".format(parsed.group(1), 
                                                 parsed.group(2),
                                                 parsed.group(3))
+                if pcic12:
+                    scenario += " (PCIC12)"
+                return scenario
             else:
                 return scenario
         
+        pcic_12 = ["BCC-CSM2-MR", "CMCC-ESM2", "EC-Earth3-Veg", "FGOALS-g3",\
+	        "INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MPI-ESM1-2-HR",\
+        	"MRI-ESM2-0", "NorESM2-LM", "TaiESM1", "UKESM1-0-LL"]
         for dfv in ensemble.data_file_variables:
+            if dfv.file.run.model.short_name in pcic_12:
+                scenario = format_scenario(dfv.file.run.emission.short_name, pcic12=True)
+                yield scenario,\
+                    dfv.file.run.model.short_name,\
+                    dfv.file.run.name,\
+                    dfv.netcdf_variable_name,\
+                    dfv.file.unique_id.replace('+', '-')
+
+            # Display all models in general headings
             yield format_scenario(dfv.file.run.emission.short_name),\
                 dfv.file.run.model.short_name,\
                 dfv.file.run.name,\
