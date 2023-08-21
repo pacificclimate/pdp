422094b43db22d19abe0336a8f9680277df62230
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2023-08-21 15:01:16 -0700

    Use test ensemble for gridded observations and add PCIC Blend dataset

--- pdp/portals/gridded_observations.py
+++ pdp/portals/gridded_observations.py
@@ -8,7 +8,7 @@ from pdp_util.ensemble_members import EnsembleMemberLister
 __all__ = ['url_base', 'mk_frontend', 'mk_backend']
 
 
-ensemble_name = 'gridded-obs-met-data'
+ensemble_name = 'gridded-obs-met-data-test'
 url_base = '/gridded_observations'
 title = 'Daily Gridded Meteorological Datasets'
 
@@ -19,7 +19,8 @@ class GriddedObservationsEnsembleLister(EnsembleMemberLister):
         dataset_names = {
             "ANUSPLIN_CDA_v2012.1": "NRCANmet 2012",
             "SYMAP_BC_v1": "PBCmet 2010",
-            "TPS_NWNA_v1": "PNWNAmet 2015"}
+            "TPS_NWNA_v1": "PNWNAmet 2015",
+            "PCIC_BLEND_v1": "PCIC Blend 2021"}
 
         for dfv in sorted(ensemble.data_file_variables,
                           key=lambda dfv: dfv.netcdf_variable_name):
